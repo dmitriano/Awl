57d6d99355a52f132b6a84073eb42576419fccb8
dmitriano <winappdev@gmail.com>
2017-09-21 01:16:01 +0300

    add Mutex

---
+++ Awl/Mutext.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <mutex>
+
+namespace awl
+{
+	struct IMutex
+	{
+		virtual void Lock() = 0;
+		virtual void Unlock() = 0;
+	};
+
+	template <class mutex>
+	class MutexWrapper : public IMutex
+	{
+	public:
+
+		MutexWrapper(mutex & wrapped) : m(wrapped)
+		{
+		}
+
+		void Lock() override
+		{
+			m.lock();
+		}
+
+		void Unlock() override
+		{
+			m.unlock();
+		}
+
+	private:
+
+		mutex & m;
+	};
+
+	//typedef Mutex<std::mutex> Mutex;
+	//typedef Mutex<std::recursive_mutex> RecursiveMutex;
+
+	class OptionalLock
+	{
+	public:
+
+		OptionalLock(IMutex * p_mutex) : pMutex(p_mutex)
+		{
+			if (pMutex != nullptr)
+			{
+				pMutex->Lock();
+			}
+		}
+
+		~OptionalLock()
+		{
+			if (pMutex != nullptr)
+			{
+				pMutex->Unlock();
+			}
+		}
+
+		void Unlock()
+		{
+			if (pMutex != nullptr)
+			{
+				pMutex->Unlock();
+
+				pMutex = nullptr;
+			}
+		}
+
+	private:
+
+		IMutex * pMutex;
+	};
+}
