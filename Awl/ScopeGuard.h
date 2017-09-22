7896a8c747b1282c11ba11be47cb3534679b1ff0
Dmitry Efimov <d.efimov@ians.aero>
2017-09-22 21:31:46 +0300

    added make_scope_guard with init/free

--- Awl/ScopeGuard.h
+++ Awl/ScopeGuard.h
@@ -7,7 +7,7 @@ namespace awl
 	{
 	public:
 
-		scope_guard(const Lambda & l) : lambda(l)
+		scope_guard(const Lambda & f, bool e = true) : free(f), engaged(e)
 		{
 		}
 
@@ -15,14 +15,14 @@ namespace awl
 
 		scope_guard & operator = (const scope_guard &) = delete;
 
-		scope_guard(scope_guard && other) : lambda(std::move(other.lambda))
+		scope_guard(scope_guard && other) : free(std::move(other.free))
 		{
 			other.engaged = false;
 		}
 
 		scope_guard & operator = (scope_guard && other)
 		{
-			lambda = std::move(other.lambda);
+			free = std::move(other.free);
 			other.engaged = false;
 		}
 
@@ -30,7 +30,7 @@ namespace awl
 		{
 			if (engaged)
 			{
-				lambda();
+				free();
 			}
 		}
 
@@ -41,14 +41,25 @@ namespace awl
 
 	private:
 
-		Lambda lambda;
+		Lambda free;
 
 		bool engaged = true;
 	};
 
 	template <class Lambda>
-	inline scope_guard<Lambda> make_scope_guard(const Lambda & l)
+	inline scope_guard<Lambda> make_scope_guard(const Lambda & free, bool engaged = true)
 	{
-		return scope_guard<Lambda>(l);
+		return scope_guard<Lambda>(free, engaged);
+	}
+
+	template <class Init, class Free>
+	inline scope_guard<Free> make_scope_guard(const Init & init, const Free & free, bool engaged = true)
+	{
+		if (engaged)
+		{
+			init();
+		}
+
+		return make_scope_guard(free, engaged);
 	}
 }
--- Tests/ScopeGuardTest.cpp
+++ Tests/ScopeGuardTest.cpp
@@ -28,4 +28,49 @@ void ScopeGuardTest()
 	}
 
 	Assert::AreEqual(usage, 0);
+
+	{
+		auto guard = awl::make_scope_guard(
+			[&usage]() { ++usage; },
+			[&usage]() { --usage; });
+
+		Assert::AreEqual(usage, 1);
+	}
+
+	Assert::AreEqual(usage, 0);
+
+	{
+		auto guard = awl::make_scope_guard(
+			[&usage]() { ++usage; },
+			[&usage]() { --usage; });
+
+		guard.release();
+
+		Assert::AreEqual(usage, 1);
+	}
+
+	Assert::AreEqual(usage, 1);
+
+	usage = 0;
+
+	//engaged = false
+
+	{
+		auto guard = awl::make_scope_guard([&usage]() { --usage; }, false);
+
+		Assert::AreEqual(usage, 0);
+	}
+
+	Assert::AreEqual(usage, 0);
+
+	{
+		auto guard = awl::make_scope_guard(
+			[&usage]() { ++usage; },
+			[&usage]() { --usage; },
+			false);
+
+		Assert::AreEqual(usage, 0);
+	}
+
+	Assert::AreEqual(usage, 0);
 }
