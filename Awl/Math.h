e68cba8164e02f5f33ecde355269a14817a41dff
dmitriano <winappdev@gmail.com>
2017-09-23 13:32:54 +0300

    added Math.h

---
+++ Awl/Math.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//#define M_PI_4     0.785398163397448309616  // pi/4
+//#define M_1_PI     0.318309886183790671538  // 1/pi
+//#define M_2_PI     0.636619772367581343076  // 2/pi
+//#define M_2_SQRTPI 1.12837916709551257390   // 2/sqrt(pi)
+//#define M_SQRT2    1.41421356237309504880   // sqrt(2)
+//#define M_SQRT1_2  0.707106781186547524401  // 1/sqrt(2)
+//#endif
+
+namespace awl { namespace math {
+
+	template <class T> constexpr T pi() { return static_cast<T>(3.14159265358979323846); }
+
+	template <class T> constexpr T half_pi() { return static_cast<T>(1.57079632679489661923); }
+
+	template <class T> constexpr T quarter_pi() { return static_cast<T>(0.785398163397448309616); }
+}}
