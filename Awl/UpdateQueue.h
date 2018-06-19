5230d43500a40b3304c4f95d11110552c5937cc2
Dmitriano <winappdev@gmail.com>
2018-06-19 15:27:43 +0300

    Made UpdateQueue mutex recursive

--- Awl/UpdateQueue.h
+++ Awl/UpdateQueue.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "Awl/QuickList.h"
+#include "Awl/ScopeGuard.h"
 
 #include <mutex>
 #include <functional>
@@ -47,47 +48,46 @@ namespace awl
 
         void ClearPending()
         {
-            std::lock_guard<std::mutex> lock(queueMutex);
+            std::lock_guard<std::recursive_mutex> lock(queueMutex);
 
-            //put them directly to FreeBlocks, but not to RenderingMessages
-            //note that using PendingMessages.clear() here will result in memory leak
-            FreeBlocks.push_back(PendingMessages);
+            //put them directly to freeBlocks, but not to renderingMessages
+            //note that using pendingMessages.clear() here will result in memory leak
+            freeBlocks.push_back(pendingMessages);
         }
 
         //called by UI thread to propagate changes to render thread
         void Push(const std::function<void(Args ...)> & func)
         {
-            std::lock_guard<std::mutex> lock(queueMutex);
+            std::lock_guard<std::recursive_mutex> lock(queueMutex);
 
             Message * p_message = nullptr;
 
-            if (FreeBlocks.empty())
+            if (freeBlocks.empty())
             {
                 p_message = new Message(func);
             }
             else
             {
-                p_message = FreeBlocks.pop_front();
+                p_message = freeBlocks.pop_front();
             }
 
             p_message->Func = func;
 
-            PendingMessages.push_back(p_message);
+            pendingMessages.push_back(p_message);
         }
 
         //called by render thread to apply changes queued by UI thread
         void ApplyUpdates(Args ... args)
         {
-            PrepareData();
+            //if an exception is thrown, the queue stays in a valid state, but remaining updates are lost
+            auto guard = awl::make_scope_guard( [this]() { PrepareData();}, [this]() { FreeData();});
 
             //we do not lock anything while applying the changes
             //so Push can be called while ApplyUpdates is still executed
-            for (typename MessageQueue::const_iterator i = RenderingMessages.begin(); i != RenderingMessages.end(); ++i)
+            for (const Message * p_m : renderingMessages)
             {
-                i->Func(args ...);
+                p_m->Func(args ...);
             }
-
-            FreeData();
         }
 
     private:
@@ -111,41 +111,42 @@ namespace awl
         //should be called on render thread after PrepareData()
         //bool IsEmpty()
         //{
-        //	return RenderingMessages.empty();
+        //	return renderingMessages.empty();
         //}
 
         void PrepareData()
         {
-            std::lock_guard<std::mutex> lock(queueMutex);
+            std::lock_guard<std::recursive_mutex> lock(queueMutex);
 
-            //moove all the messages from PendingMessages to RenderingMessages
+            //moove all the messages from pendingMessages to renderingMessages
             //very short operation that actually performs a few assignments
-            RenderingMessages = std::move(PendingMessages);
+            renderingMessages = std::move(pendingMessages);
         }
 
         void FreeData()
         {
-            std::lock_guard<std::mutex> lock(queueMutex);
+            std::lock_guard<std::recursive_mutex> lock(queueMutex);
 
-            for (Message * p_message : RenderingMessages)
+            for (Message * p_message : renderingMessages)
             {
                 //release lambda expression
                 p_message->Func = std::function<void(Args ...)>();
             }
 
-            //recycle RenderingMessages (also very short operation)
-            FreeBlocks.push_back(RenderingMessages);
+            //recycle renderingMessages (also very short operation)
+            freeBlocks.push_back(renderingMessages);
         }
 
-        std::mutex queueMutex;
+        //an update can queue another updates by calling Push() while it is processed, so the mutex is recursive
+        std::recursive_mutex queueMutex;
 
         //messages (changes) queued by UI thread while rendering operation is in progress
-        MessageQueue PendingMessages;
+        MessageQueue pendingMessages;
 
         //we recycle freed queue elements to avoid superfluous dynamic memory allocation
-        MessageQueue FreeBlocks;
+        MessageQueue freeBlocks;
 
         //messages (changes) that render thread is applying at the moment
-        MessageQueue RenderingMessages;
+        MessageQueue renderingMessages;
     };
 }
