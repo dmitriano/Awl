169516c10b76bf8a6553de0f2da94070e007bd67
Dmitriano <winappdev@gmail.com>
2018-08-15 23:11:44 +0300

    Shorten the code with std::false_type/std::true_type.

--- Awl/TypeTraits.h
+++ Awl/TypeTraits.h
@@ -7,36 +7,21 @@
 namespace awl
 {
     template <typename T>
-    struct is_collection
-    {
-        static const bool value = false;
-    };
+    struct is_collection : std::false_type {};
 
     template <class T, class Alloc>
-    struct is_collection<std::vector<T, Alloc>>
-    {
-        static const bool value = true;
-    };
+    struct is_collection<std::vector<T, Alloc>> : std::true_type {};
 
     template <class T, class Compare, class Alloc>
-    struct is_collection<std::set<T, Compare, Alloc>>
-    {
-        static const bool value = true;
-    };
+    struct is_collection<std::set<T, Compare, Alloc>> : std::true_type {};
 
-    static_assert(is_collection<std::vector<int>>::value, "std::vector<int> is not a container.");
+    static_assert(is_collection<std::vector<int>>::value, "std::vector<int> is not a collection.");
 
     template <typename T, typename S>
-    struct is_string
-    {
-        static const bool value = false;
-    };
+    struct is_string : std::false_type{};
 
     template <class T, class Traits, class Alloc>
-    struct is_string<T, std::basic_string<T, Traits, Alloc>>
-    {
-        static const bool value = true;
-    };
+    struct is_string<T, std::basic_string<T, Traits, Alloc>> : std::true_type {};
 
     static_assert(is_string<char, std::string>::value, "is_string<char, std::string>::value is false.");
     static_assert(!is_string<char, std::wstring>::value, "is_string<char, std::wstring>::value is true.");
