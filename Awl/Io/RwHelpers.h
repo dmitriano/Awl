f262c43d6b775585d47b2472a0cb4e04b35252c5
Dmitriano <winappdev@gmail.com>
2018-08-22 00:01:28 +0300

    Small code style corrections.

--- Awl/Io/RwHelpers.h
+++ Awl/Io/RwHelpers.h
@@ -25,14 +25,14 @@ namespace awl
         }
 
         //Initially on Windows the type was long long, but Android build fails with it, so I changed it to int64_t, what is the difference between them?
-        static_assert(std::is_same <std::chrono::nanoseconds::rep, int64_t >::value, "nanoseconds::rep is not int64_t");
+        static_assert(std::is_same <std::chrono::nanoseconds::rep, int64_t>::value, "nanoseconds::rep is not int64_t");
 
         template <class Stream, class Clock, class Duration>
         inline void ReadScalar(Stream & s, std::chrono::time_point<Clock, Duration> & val)
         {
             using namespace std::chrono;
 
-            long long ns_count;
+            int64_t ns_count;
 
             ReadScalar(s, ns_count);
 
@@ -46,7 +46,7 @@ namespace awl
 
             auto ns = duration_cast<nanoseconds>(val.time_since_epoch());
 
-            long long ns_count = ns.count();
+            int64_t ns_count = ns.count();
 
             WriteScalar(s, ns_count);
         }
@@ -54,7 +54,7 @@ namespace awl
         template <class Stream, typename Char>
         void ReadString(Stream & s, std::basic_string<Char> & val)
         {
-            size_t len;
+            std::basic_string<Char>::size_type len;
 
             ReadScalar(s, len);
 
@@ -68,7 +68,7 @@ namespace awl
         template <class Stream, typename Char>
         void WriteString(Stream & s, const std::basic_string<Char> & val)
         {
-            const size_t len = val.length();
+            std::basic_string<Char>::size_type len = val.length();
 
             WriteScalar(s, len);
 
@@ -78,7 +78,7 @@ namespace awl
         template <class Stream, typename T>
         inline void ReadVector(Stream & s, std::vector<T> & items)
         {
-            int size = sizeof(T);
+            std::vector<T>::size_type size = sizeof(T);
 
             s.Read(reinterpret_cast<uint8_t *>(items.data()), size * items.size());
         }
@@ -86,7 +86,7 @@ namespace awl
         template <class Stream, typename T>
         inline void WriteVector(Stream & s, const std::vector<T> & items)
         {
-            int size = sizeof(T);
+            std::vector<T>::size_type size = sizeof(T);
 
             s.Write(reinterpret_cast<const uint8_t *>(items.data()), size * items.size());
         }
@@ -116,9 +116,9 @@ namespace awl
 
             for (std::vector<bool>::size_type i = 0; i < n;)
             {
-                unsigned char aggr = 0;
+                uint8_t aggr = 0;
 
-                for (unsigned char mask = 1; mask > 0 && i < n; ++i, mask <<= 1)
+                for (uint8_t mask = 1; mask > 0 && i < n; ++i, mask <<= 1)
                 {
                     if (x.at(i))
                     {
