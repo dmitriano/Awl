05c8c95ca4d21b253e630612e22e16280ed167aa
Dmitriano <winappdev@gmail.com>
2018-08-21 23:25:07 +0300

    Vector Read/Write test.

--- Awl/Io/RwHelpers.h
+++ Awl/Io/RwHelpers.h
@@ -1,13 +1,14 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace awl 
 {
     namespace io
     {
         template <class Stream, typename T>
-        void ReadScalar(Stream & s, T & val)
+        inline void ReadScalar(Stream & s, T & val)
         {
             const size_t size = sizeof(T);
 
@@ -15,7 +16,7 @@ namespace awl
         }
 
         template <class Stream, typename T>
-        void WriteScalar(Stream & s, const T & val)
+        inline void WriteScalar(Stream & s, const T & val)
         {
             const size_t size = sizeof(T);
 
@@ -44,6 +45,61 @@ namespace awl
             WriteScalar(s, len);
 
             s.Write(reinterpret_cast<const uint8_t *>(val.data()), len * sizeof(Char));
+        }
+
+        template <class Stream, typename T>
+        inline void ReadVector(Stream & s, std::vector<T> & items)
+        {
+            int size = sizeof(T);
+
+            s.Read(reinterpret_cast<uint8_t *>(items.data()), size * items.size());
+        }
+
+        template <class Stream, typename T>
+        inline void WriteVector(Stream & s, const std::vector<T> & items)
+        {
+            int size = sizeof(T);
+
+            s.Write(reinterpret_cast<const uint8_t *>(items.data()), size * items.size());
+        }
+
+        template <class Stream>
+        void ReadVector(Stream & s, std::vector<bool> & x)
+        {
+            std::vector<bool>::size_type n = x.size();
+
+            for (std::vector<bool>::size_type i = 0; i < n;)
+            {
+                uint8_t aggr;
+
+                ReadScalar(s, aggr);
+
+                for (unsigned char mask = 1; mask > 0 && i < n; ++i, mask <<= 1)
+                {
+                    x.at(i) = (aggr & mask) != 0;
+                }
+            }
+        }
+
+        template <class Stream>
+        void WriteVector(Stream & s, const std::vector<bool> & x)
+        {
+            std::vector<bool>::size_type n = x.size();
+
+            for (std::vector<bool>::size_type i = 0; i < n;)
+            {
+                unsigned char aggr = 0;
+
+                for (unsigned char mask = 1; mask > 0 && i < n; ++i, mask <<= 1)
+                {
+                    if (x.at(i))
+                    {
+                        aggr |= mask;
+                    }
+                }
+
+                WriteScalar(s, aggr);
+            }
         }
     }
 }
--- Tests/RwHelpersTest.cpp
+++ Tests/RwHelpersTest.cpp
@@ -86,4 +86,47 @@ AWL_TEST(IoStringReadWrite)
 {
     TestString(context, std::string("some sample string"));
     TestString(context, std::wstring(L"some sample string"));
+
+    TestString(context, std::string("a"));
+    TestString(context, std::wstring(L"a"));
+
+    TestString(context, std::string(""));
+    TestString(context, std::wstring(L""));
+}
+
+template <class T>
+static void TestVector(const TestContext & context, std::vector<T> sample)
+{
+    AWL_ATTRIBUTE(size_t, iteration_count, 10);
+
+    std::vector<uint8_t> v;
+
+    VectorOutputStream out(v);
+
+    for (int i = 0; i < iteration_count; ++i)
+    {
+        WriteVector(out, sample);
+    }
+
+    VectorInputStream in(v);
+
+    for (int i = 0; i < iteration_count; ++i)
+    {
+        std::vector<T> result;
+
+        result.resize(sample.size());
+
+        ReadVector(in, result);
+
+        Assert::IsTrue(sample == result);
+    }
+
+    Assert::IsTrue(in.End());
+}
+
+AWL_TEST(IoVectorReadWrite)
+{
+    TestVector(context, std::vector<int>{0, 1, 2, 3, 4, 5});
+    TestVector(context, std::vector<double>{0.0, 1.0, 2.0, 3.0, 4.0, 5.0});
+    TestVector(context, std::vector<bool>{true, false, true, false, true, false});
 }
