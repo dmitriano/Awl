c5db201ca5e94b59fde4916b553bcf7ddfb82bdd
Dmitriano <winappdev@gmail.com>
2018-09-06 18:08:07 +0300

    Added clamp.

---
+++ Awl/Algorithm.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <algorithm>
+
+namespace awl
+{
+    //std::clamp in C++17 is a bit different, it is constexpr and returns const T&
+
+    template<class T, class Compare>
+    inline void clamp(T& v, const T& lo, const T& hi, Compare less)
+    {
+        if (less(v, lo))
+        {
+            v = lo;
+        }
+        else
+        {
+            if (less(hi, v))
+            {
+                v = hi;
+            }
+        }
+    }
+
+    template<class T>
+    inline void clamp(T& v, const T& lo, const T& hi)
+    {
+        clamp(v, lo, hi, std::less<>());
+    }
+}
--- Awl/Math.h
+++ Awl/Math.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 //#define M_PI_4     0.785398163397448309616  // pi/4
 //#define M_1_PI     0.318309886183790671538  // 1/pi
 //#define M_2_PI     0.636619772367581343076  // 2/pi
@@ -8,25 +10,37 @@
 //#define M_SQRT1_2  0.707106781186547524401  // 1/sqrt(2)
 //#endif
 
-namespace awl {
-    namespace math {
+namespace awl
+{
+    namespace math
+    {
+        template <class T> constexpr typename std::enable_if<std::is_floating_point<T>::value, T>::type pi() { return static_cast<T>(3.14159265358979323846); }
 
-        template <class T> constexpr T pi() { return static_cast<T>(3.14159265358979323846); }
+        template <class T> constexpr typename std::enable_if<std::is_floating_point<T>::value, T>::type half_pi() { return static_cast<T>(1.57079632679489661923); }
 
-        template <class T> constexpr T half_pi() { return static_cast<T>(1.57079632679489661923); }
-
-        template <class T> constexpr T quarter_pi() { return static_cast<T>(0.785398163397448309616); }
+        template <class T> constexpr typename std::enable_if<std::is_floating_point<T>::value, T>::type quarter_pi() { return static_cast<T>(0.785398163397448309616); }
 
         template <class T>
-        inline T degrees_to_radians(T rad)
+        constexpr typename std::enable_if<std::is_floating_point<T>::value, T>::type degrees_to_radians(T rad)
         {
             return rad / static_cast<T>(180.0L) * pi<T>();
         }
 
         template <class T>
-        inline T raidans_to_degrees(T deg)
+        constexpr typename std::enable_if<std::is_floating_point<T>::value, T>::type raidans_to_degrees(T deg)
         {
             return deg / pi<T>() * static_cast<T>(180.0L);
+        }
+
+        //GCC 4.9 does not have std::round and std::lround
+        template <class I, class F>
+        inline typename std::enable_if<std::is_integral<I>::value && std::is_floating_point<F>::value, I>::type round(F f)
+        {
+#if (__GNUC__ == 4 && __GNUC_MINOR__ <= 9)
+            return static_cast<I>(f + 0.5f);
+#else
+            return static_cast<I>(std::lruond(f));
+#endif
         }
     }
 }
