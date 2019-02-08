685cdd4a96d08970a2b74beedefb12efe5f48e0d
Dmitriano <winappdev@gmail.com>
2019-02-08 11:13:44 -0800

    Added BitMap.

---
+++ Awl/BitMap.h
@@ -0,0 +1,181 @@
+#pragma once
+
+#include <type_traits>
+#include <limits>
+#include <bitset>
+#include <initializer_list>
+
+namespace awl
+{
+    template <class T> struct EnumTraits;
+
+    template<typename Enum, bool IsEnum = std::is_enum<Enum>::value>
+    class bitmap;
+
+    template<typename Enum>
+    class bitmap<Enum, true>
+    {
+    private:
+
+        constexpr const static std::size_t N = EnumTraits<Enum>::count();
+        typedef std::bitset<N> BitSet;
+
+    public:
+
+        constexpr bitmap() = default;
+
+        constexpr bitmap(std::initializer_list<Enum> values)
+        {
+            for (Enum v : values)
+            {
+                m_bits.set(Enum2Index(v), true);
+            }
+        }
+
+        constexpr bitmap operator|(Enum value) const
+        {
+            bitmap bm = *this;
+            bm.m_bits |= Enum2Bits(value);
+            return bm;
+        }
+
+        constexpr bitmap operator&(Enum value) const
+        {
+            bitmap bm = *this;
+            bm.m_bits &= Enum2Bits(value);
+            return bm;
+        }
+
+        constexpr bitmap operator^(Enum value) const
+        {
+            bitmap bm = *this;
+            bm.m_bits ^= Enum2Bits(value);
+            return bm;
+        }
+
+        constexpr bitmap operator~() const
+        {
+            bitmap bm = *this;
+            bm.m_bits.flip();
+            return bm;
+        }
+
+        constexpr bitmap& operator|=(Enum value)
+        {
+            m_bits |= Enum2Bits(value);
+            return *this;
+        }
+
+        constexpr bitmap& operator&=(Enum value)
+        {
+            m_bits &= Enum2Bits(value);
+            return *this;
+        }
+
+        constexpr bitmap& operator^=(Enum value)
+        {
+            m_bits ^= Enum2Bits(value);
+            return *this;
+        }
+
+        constexpr bitmap operator|(const bitmap & other) const
+        {
+            bitmap bm = *this;
+            bm.m_bits |= other.m_bits;
+            return bm;
+        }
+
+        constexpr bitmap operator&(const bitmap & other) const
+        {
+            bitmap bm = *this;
+            bm.m_bits &= other.m_bits;
+            return bm;
+        }
+
+        constexpr bitmap operator^(const bitmap & other) const
+        {
+            bitmap bm = *this;
+            bm.m_bits ^= other.m_bits;
+            return bm;
+        }
+
+        constexpr bitmap& operator|=(const bitmap & other)
+        {
+            m_bits |= other.m_bits;
+            return *this;
+        }
+
+        constexpr bitmap& operator&=(const bitmap & other)
+        {
+            m_bits &= other.m_bits;
+            return *this;
+        }
+
+        constexpr bitmap& operator^=(const bitmap & other)
+        {
+            m_bits ^= other.m_bits;
+            return *this;
+        }
+
+        constexpr bool test(Enum value) const
+        {
+            return m_bits.test(Enum2Index(value));
+        }
+
+        constexpr bool any() const { return m_bits.any(); }
+        
+        constexpr bool all() const { return m_bits.all(); }
+        
+        constexpr bool none() const { return m_bits.none(); }
+        
+        constexpr bitmap& set(Enum value, bool b)
+        {
+            m_bits.set(Enum2Index(value), b);
+
+            return *this;
+        }
+
+        constexpr bool operator[](Enum value) const
+        {
+            return m_bits[Enum2Index(value)];
+        }
+
+        auto operator[](Enum value)
+        {
+            return m_bits[Enum2Index(value)];
+        }
+
+        constexpr operator bool() const { return any(); }
+
+    private:
+
+        static constexpr std::size_t Enum2Index(Enum value)
+        {
+            return static_cast<std::size_t>(value);
+        }
+
+        static constexpr BitSet Enum2Bits(Enum value)
+        {
+            BitSet bits;
+            bits.set(Enum2Index(value));
+            return bits;
+        }
+
+        BitSet m_bits;
+    };
+}
+
+#define AWL_BITMAP(EnumName, ...) \
+    enum class EnumName { __VA_ARGS__ }; \
+    template<> \
+    struct awl::EnumTraits<EnumName> \
+    { \
+    private: \
+        enum { __VA_ARGS__ }; \
+    public: \
+        static constexpr std::size_t count() \
+        { \
+            auto values = { __VA_ARGS__ }; \
+            return static_cast<std::size_t>(values.end() - values.begin()); \
+        } \
+    };
---
+++ Tests/BitMapTest.cpp
@@ -0,0 +1,61 @@
+#include "Awl/BitMap.h"
+
+AWL_BITMAP(Vehicle, Car, Train, AirPlain)
+
+static_assert(awl::EnumTraits<Vehicle>::count() == 3);
+
+#include "Awl/Testing/UnitTest.h"
+
+using namespace awl::testing;
+
+AWL_TEST(BitMap)
+{
+    AWL_UNUSED_CONTEXT;
+
+    const awl::bitmap<Vehicle> car{ Vehicle::Car };
+    
+    Assert::IsTrue(car);
+    
+    Assert::IsTrue(car[Vehicle::Car]);
+    Assert::IsTrue(car & Vehicle::Car);
+    Assert::IsTrue(car | Vehicle::Car);
+    Assert::IsFalse(car ^ Vehicle::Car);
+
+    Assert::IsFalse(car[Vehicle::Train]);
+    Assert::IsFalse(car & Vehicle::Train);
+    Assert::IsFalse(car & Vehicle::Train);
+    Assert::IsTrue(car | Vehicle::Train);
+    Assert::IsTrue((car ^ Vehicle::Train) & Vehicle::Car);
+
+    const awl::bitmap<Vehicle> other = ~car;
+
+    Assert::IsFalse(other[Vehicle::Car]);
+    Assert::IsFalse(other & Vehicle::Car);
+    Assert::IsTrue(other | Vehicle::Car);
+    Assert::IsTrue(other ^ Vehicle::Car);
+
+    Assert::IsTrue(other[Vehicle::Train] && other[Vehicle::AirPlain]);
+    Assert::IsTrue((other & Vehicle::Train) && (other & Vehicle::AirPlain));
+    Assert::IsFalse(other & Vehicle::Car);
+    Assert::IsTrue(other | Vehicle::Car);
+    Assert::IsFalse((other ^ Vehicle::Train) & Vehicle::Car);
+
+    const awl::bitmap<Vehicle> all = car | other;
+    Assert::IsTrue(all.all());
+    Assert::IsTrue(all == (car ^ other));
+    Assert::IsFalse(car & other);
+
+    const awl::bitmap<Vehicle> none = ~all;
+    Assert::IsTrue(none.none());
+    Assert::IsFalse(none);
+
+    Assert::IsTrue(all == ~none);
+
+    awl::bitmap<Vehicle> var;
+    Assert::IsFalse(var);
+    var[Vehicle::Car] = true;
+    Assert::IsTrue(var[Vehicle::Car]);
+    Assert::IsFalse(var[Vehicle::Train]);
+    Assert::IsFalse(var[Vehicle::AirPlain]);
+    Assert::IsTrue(var == car);
+}
