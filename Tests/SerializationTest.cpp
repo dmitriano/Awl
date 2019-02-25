5cdc18b92318b0e478a8bdc36fbe9b24b7480130
Dmitriano <winappdev@gmail.com>
2019-02-25 06:39:59 -0800

    Sample implementation of is_class_fixed_size().

--- Tests/SerializationTest.cpp
+++ Tests/SerializationTest.cpp
@@ -1,6 +1,8 @@
 #include "Awl/Serializable.h"
 #include "Awl/Testing/UnitTest.h"
 
+#include <string>
+
 using namespace awl::testing;
 
 namespace awl
@@ -16,6 +18,34 @@ namespace awl
 
     template <typename T>
     inline constexpr bool is_tuplizable = std::is_class<T>::value && is_tuplizable_t<T>::value;
+
+    //MSVC 2017: error C2131: expression did not evaluate to a constant.
+    /*
+    template <class T, typename Func>
+    inline constexpr void for_each_member(const T & val, const Func & f)
+    {
+        static_assert(std::is_arithmetic<T>::value || is_tuplizable<T>);
+
+        if constexpr (std::is_arithmetic<T>::value)
+        {
+            f(val);
+        }
+        else if constexpr (is_tuplizable<T>)
+        {
+            for_each(object_as_tuple(val), f);
+        }
+    }
+
+    template <class T>
+    constexpr std::size_t sizeof_object(const T & val)
+    {
+        std::size_t size = 0;
+
+        for_each_member(val, [&size](auto& field) { size += sizeof_object(field); });
+
+        return size;
+    }
+    */
 
     template <class T>
     constexpr std::size_t sizeof_object(const T & val)
@@ -44,6 +74,41 @@ namespace awl
         return sizeof_object(ConstT{});
     }
 
+    template <class T>
+    constexpr bool is_object_fixed_size(const T & val)
+    {
+        if constexpr (std::is_arithmetic<T>::value)
+        {
+            static_cast<void>(val);
+            return true;
+        }
+        else if constexpr (is_tuplizable<T>)
+        {
+            bool all = true;
+
+            for_each(object_as_tuple(val), [&all](auto& field)
+            {
+                if (!is_object_fixed_size(field))
+                {
+                    all = false;
+                }
+            });
+
+            return all;
+        }
+
+        static_cast<void>(val);
+        return false;
+    }
+
+    template <class T>
+    constexpr bool is_class_fixed_size()
+    {
+        using ConstT = const T;
+
+        return is_object_fixed_size(ConstT{});
+    }
+
     namespace static_test
     {
         struct A
@@ -62,9 +127,22 @@ namespace awl
             AWL_SERIALIZABLE(a, z)
         };
 
+        struct C
+        {
+            B b;
+            //std::string s;
+            int * p;
+
+            AWL_SERIALIZABLE(b, p)
+        };
+
         static_assert(is_tuplizable<A>);
 
         static_assert(sizeof_class<A>() == sizeof(bool) + sizeof(int));
         static_assert(sizeof_class<B>() == sizeof_class<A>() + sizeof(double));
+
+        static_assert(is_class_fixed_size<A>());
+        static_assert(is_class_fixed_size<B>());
+        static_assert(!is_class_fixed_size<C>());
     }
 }
