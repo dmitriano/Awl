260f575b4c8d5afca07399f41f5e36536b2474ad
Dmitriano <winappdev@gmail.com>
2019-05-22 11:07:33 -0700

    added IoStream

---
+++ Awl/Io/Stream.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "SequentialStream.h" 
+
+#include <cstddef>
+
+namespace awl
+{
+    namespace io
+    {
+        class StreamPointer
+        {
+        public:
+
+            virtual size_t GetPosition() = 0;
+
+            virtual void Seek(std::size_t pos, bool begin = true) = 0;
+
+            virtual void Move(std::ptrdiff_t offset) = 0;
+
+            virtual size_t GetLength() = 0;
+
+            virtual ~StreamPointer() = default;
+        };
+
+        class InputStream : public SequentialInputStream, public virtual StreamPointer
+        {
+        };
+
+        class OutputStream : public SequentialOutputStream, public virtual StreamPointer
+        {
+        public:
+
+            virtual void Flush() = 0;
+
+            virtual void Truncate() = 0;
+        };
+
+        class IoStream : public InputStream, public OutputStream
+        {
+        };
+    }
+}
