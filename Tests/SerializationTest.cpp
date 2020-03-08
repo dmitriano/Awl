74dfba18f7cc4d9190cb4a68836c914537da3812
dmitriano <winappdev@gmail.com>
2020-03-09 02:10:37 +0300

    experimentation with type_info

--- Tests/SerializationTest.cpp
+++ Tests/SerializationTest.cpp
@@ -1,5 +1,6 @@
 #include "Awl/Serializable.h"
 #include "Awl/Testing/UnitTest.h"
+#include "Awl/Io/RwHelpers.h"
 
 #include <string>
 
@@ -140,3 +141,79 @@ namespace awl
         static_assert(!is_class_fixed_size<C>());
     }
 }
+
+namespace awl
+{
+    class type_info
+    {
+    public:
+
+        constexpr type_info(const char * name) : m_name(name)
+        {
+        }
+
+        constexpr const char * name() const noexcept
+        {
+            return m_name;
+        }
+
+        constexpr bool operator == (const type_info & other)
+        {
+            const char * a = name();
+            const char * b = other.name();
+
+            while (*a != 0 && *b != 0)
+            {
+                if (*a++ != *b++)
+                {
+                    return false;
+                }
+            }
+
+            return *a == 0 && *b == 0;
+        }
+
+    private:
+
+        const char * m_name;
+
+        friend constexpr const char * make_type_info();
+    };
+    
+    template <class T>
+    constexpr const char * get_arithmetic_size()
+    {
+        switch (sizeof(T))
+        {
+        case 1: return "1";
+        case 2: return "2";
+        case 4: return "4";
+        case 8: return "8";
+        case 16: return "16";
+        default: static_assert(dependent_false_v<T>);
+        }
+    }
+    
+    template <class T>
+    constexpr std::enable_if_t<std::is_arithmetic_v<T>, type_info> make_type_info()
+    {
+        const char * prefix = std::is_signed_v<T> ? "int" : "uint";
+        return type_info(prefix);// +get_arithmetic_size<T>());
+    }
+
+    //template <class T, class Allocator = std::allocator<T>>
+    //constexpr type_info make_type_info(std::vector<T, Allocator> & v)
+    //{
+    //    static_cast<void>(val);
+    //    return "vector" + make_type_info;
+    //};
+
+    static_assert(awl::make_type_info<int>() == awl::make_type_info<long>());
+}
+
+/*
+AWT_TEST(TypeName)
+{
+    context.out << awl::FromACString(awl::type_info<int>().name()) << std::endl;
+}
+*/
