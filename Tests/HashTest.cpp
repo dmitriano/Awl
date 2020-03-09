5380e0abc41d364618ef03bba965882b3171af8e
dmitriano <winappdev@gmail.com>
2020-03-09 16:33:37 +0300

    code alignment

--- Tests/HashTest.cpp
+++ Tests/HashTest.cpp
@@ -23,121 +23,6 @@
 using namespace awl::testing;
 using namespace awl::testing::helpers;
 
-template <class Hash>
-static void CalcHash(const TestContext & context, const awl::Char * type_name = nullptr)
-{
-    const Hash hash;
-
-    {
-        auto r = awl::make_int_range<uint8_t>(0, 1);
-
-        auto zero_val = hash(r.begin(), r.end());
-
-        AWT_ASSERT_FALSE(zero_val == typename Hash::value_type{});
-    }
-
-    AWT_ATTRIBUTE(size_t, vector_size, 1000000);
-    AWT_ATTRIBUTE(size_t, iteration_count, 100);
-
-    std::unique_ptr<uint8_t[]> p_buffer(new uint8_t[vector_size]);
-
-    for (size_t i = 0; i < vector_size; ++i)
-    {
-        p_buffer[i] = static_cast<uint8_t>(i);
-    }
-
-    typename Hash::value_type val = {};
-
-    {
-        awl::StopWatch w;
-
-        for (size_t i = 0; i < iteration_count; ++i)
-        {
-            typename Hash::value_type new_val = hash(p_buffer.get(), p_buffer.get() + vector_size);
-
-            if (val == typename Hash::value_type{})
-            {
-                val = new_val;
-            }
-            else
-            {
-                Assert::IsTrue(new_val == val);
-            }
-        }
-
-        if (type_name == nullptr)
-        {
-            context.out << awl::FromACString(typeid(hash).name());
-        }
-        else
-        {
-            context.out << type_name;
-        }
-
-        context.out << _T(": ");
-
-        ReportSpeed(context, w, vector_size * iteration_count * sizeof(uint8_t));
-
-        context.out << _T(" Hash : ") << val << std::endl;
-    }
-}
-
-AWT_TEST(Hash)
-{
-    AWT_UNUSED_CONTEXT;
-
-    using namespace awl::crypto;
-
-    const Crc64 hash(0);
-
-    {
-        std::string sample("123456789");
-
-        const Crc64::value_type sample_val = { 0xe9, 0xc6, 0xd9, 0x14, 0xc4, 0xb8, 0xd9, 0xca };
-
-        const Crc64::value_type val = hash(sample.begin(), sample.end());
-
-        Assert::IsTrue(val == sample_val);
-    }
-
-    {
-        std::set<int> set{ 1, 2, 3 };
-
-        hash(set.begin(), set.end());
-    }
-}
-
-AWT_BENCHMARK(HashPerformance)
-{
-    using namespace awl::crypto;
-
-    CalcHash<Crc64>(context, _T("Crc64"));
-
-#ifdef AWL_OPENSSL
-
-    CalcHash<Md5>(context, _T("Md5"));
-    CalcHash<Sha1>(context, _T("Sha1"));
-    CalcHash<Sha256>(context, _T("Sha256"));
-    CalcHash<Sha512>(context, _T("Sha512"));
-
-#endif
-}
-
-AWT_TEST(Hash_ToFromArray)
-{
-    AWT_UNUSED_CONTEXT;
-
-    using namespace awl::crypto;
-
-    constexpr uint64_t sample = UINT64_C(0xab28ecb46814fe75);
-
-    const auto a = to_array(sample);
-
-    const auto val = from_array<uint64_t>(a);
-
-    AWT_ASSERT_EQUAL(sample, val);
-}
-
 namespace
 {
     //StringHash can be used with switch operator.
@@ -228,11 +113,124 @@ namespace
     constexpr char sampleString[] = "123456789";
     constexpr awl::crypto::HashValue<8> sampleHash = { 0xe9, 0xc6, 0xd9, 0x14, 0xc4, 0xb8, 0xd9, 0xca };
     constexpr Int64Hash::value_type sampleIntHash = 0xe9c6d914c4b8d9ca;
-    
+
     static_assert(StringHash<Int64Hash>(0)(sampleString) == sampleIntHash);
-    
+
     //Why isn't it a constexpr?
     //static_assert(StringHash<awl::crypto::Crc64>(0)("123456789") == sampleHash);
+}
+
+template <class Hash>
+static void CalcHash(const TestContext & context, const awl::Char * type_name = nullptr)
+{
+    const Hash hash;
+
+    {
+        auto r = awl::make_int_range<uint8_t>(0, 1);
+
+        auto zero_val = hash(r.begin(), r.end());
+
+        AWT_ASSERT_FALSE(zero_val == typename Hash::value_type{});
+    }
+
+    AWT_ATTRIBUTE(size_t, vector_size, 1000000);
+    AWT_ATTRIBUTE(size_t, iteration_count, 100);
+
+    std::unique_ptr<uint8_t[]> p_buffer(new uint8_t[vector_size]);
+
+    for (size_t i = 0; i < vector_size; ++i)
+    {
+        p_buffer[i] = static_cast<uint8_t>(i);
+    }
+
+    typename Hash::value_type val = {};
+
+    {
+        awl::StopWatch w;
+
+        for (size_t i = 0; i < iteration_count; ++i)
+        {
+            typename Hash::value_type new_val = hash(p_buffer.get(), p_buffer.get() + vector_size);
+
+            if (val == typename Hash::value_type{})
+            {
+                val = new_val;
+            }
+            else
+            {
+                Assert::IsTrue(new_val == val);
+            }
+        }
+
+        if (type_name == nullptr)
+        {
+            context.out << awl::FromACString(typeid(hash).name());
+        }
+        else
+        {
+            context.out << type_name;
+        }
+
+        context.out << _T(": ");
+
+        ReportSpeed(context, w, vector_size * iteration_count * sizeof(uint8_t));
+
+        context.out << _T(" Hash : ") << val << std::endl;
+    }
+}
+
+AWT_TEST(Hash)
+{
+    AWT_UNUSED_CONTEXT;
+
+    using namespace awl::crypto;
+
+    const Crc64 hash(0);
+
+    {
+        std::string sample(sampleString);
+
+        const Crc64::value_type val = hash(sample.begin(), sample.end());
+
+        Assert::IsTrue(val == sampleHash);
+    }
+
+    {
+        std::set<int> set{ 1, 2, 3 };
+
+        hash(set.begin(), set.end());
+    }
+}
+
+AWT_BENCHMARK(HashPerformance)
+{
+    using namespace awl::crypto;
+
+    CalcHash<Crc64>(context, _T("Crc64"));
+
+#ifdef AWL_OPENSSL
+
+    CalcHash<Md5>(context, _T("Md5"));
+    CalcHash<Sha1>(context, _T("Sha1"));
+    CalcHash<Sha256>(context, _T("Sha256"));
+    CalcHash<Sha512>(context, _T("Sha512"));
+
+#endif
+}
+
+AWT_TEST(Hash_ToFromArray)
+{
+    AWT_UNUSED_CONTEXT;
+
+    using namespace awl::crypto;
+
+    constexpr uint64_t sample = UINT64_C(0xab28ecb46814fe75);
+
+    const auto a = to_array(sample);
+
+    const auto val = from_array<uint64_t>(a);
+
+    AWT_ASSERT_EQUAL(sample, val);
 }
 
 AWT_TEST(Hash_String)
