47eaf6c9af1653f0fc5dc046a862fee2d26904ce
dmitriano <winappdev@gmail.com>
2020-05-06 21:20:23 +0300

    a small code alignment

--- Awl/Io/MpHelpers.h
+++ Awl/Io/MpHelpers.h
@@ -8,14 +8,17 @@
 
 namespace awl::io::helpers
 {
-    template <class V, class T, std::size_t... index>
-    constexpr bool variant_contains_impl(std::index_sequence<index...>)
+    namespace impl
     {
-        return (std::is_same_v<std::variant_alternative_t<index, V>, T> || ...);
+        template <class V, class T, std::size_t... index>
+        constexpr bool variant_contains(std::index_sequence<index...>)
+        {
+            return (std::is_same_v<std::variant_alternative_t<index, V>, T> || ...);
+        }
     }
 
     template <class V, class T>
-    constexpr inline bool variant_contains = variant_contains_impl<V, T>(std::make_index_sequence<std::variant_size_v<V>>());
+    constexpr inline bool variant_contains = impl::variant_contains<V, T>(std::make_index_sequence<std::variant_size_v<V>>());
 
     static_assert(variant_contains<std::variant<int, bool, double>, int>);
     static_assert(!variant_contains<std::variant<int, bool, double>, float>);
