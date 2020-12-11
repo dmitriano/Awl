ac2bed86b75e0e3fd6241142cbfd93f742aec14b
dmitriano <winappdev@gmail.com>
2020-12-12 00:46:20 +0300

    SharedSingleton

--- Tests/SingletonTest.cpp
+++ Tests/SingletonTest.cpp
@@ -4,7 +4,38 @@
 
 #include "Helpers/NonCopyable.h"
 
-using A = awl::testing::helpers::NonCopyable;
+#include <memory>
+
+namespace
+{
+    constexpr int value = 5;
+    
+    using A = awl::testing::helpers::NonCopyable;
+
+    template <typename T>
+    inline bool is_uninitialized(std::weak_ptr<T> const& weak)
+    {
+        using wt = std::weak_ptr<T>;
+        return !weak.owner_before(wt{}) && !wt{}.owner_before(weak);
+    }
+
+    std::shared_ptr<A> GetObject()
+    {
+        static std::weak_ptr<A> wp;
+
+        if (is_uninitialized(wp))
+        {
+            //We have a long leaving std::weak_ptr so we do not use std::make_shared.
+            std::shared_ptr<A> p(new A(value));
+
+            wp = p;
+
+            return p;
+        }
+
+        return wp.lock();
+    }
+}
 
 AWT_TEST(Destructible)
 {
@@ -13,11 +44,37 @@ AWT_TEST(Destructible)
     AWT_ASSERT_EQUAL(0, A::count);
 
     {
-        awl::Destructible<A> da(5);
+        awl::Destructible<A> da(value);
 
         auto guard = awl::make_scope_guard([&da]() { da.Destroy();  });
 
-        AWT_ASSERT(*da == A(5));
+        AWT_ASSERT(*da == A(value));
+    }
+
+    AWT_ASSERT_EQUAL(0, A::count);
+}
+
+AWT_TEST(SharedSingleton)
+{
+    AWT_UNUSED_CONTEXT;
+
+    AWT_ASSERT_EQUAL(0, A::count);
+
+    {
+        auto p1 = GetObject();
+
+        AWT_ASSERT_EQUAL(1, A::count);
+
+        {
+            auto p2 = GetObject();
+
+            AWT_ASSERT_EQUAL(1, A::count);
+
+            AWT_ASSERT(*p1 == A(value));
+            AWT_ASSERT(*p2 == A(value));
+        }
+
+        AWT_ASSERT_EQUAL(1, A::count);
     }
 
     AWT_ASSERT_EQUAL(0, A::count);
