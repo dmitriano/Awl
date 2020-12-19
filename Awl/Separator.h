b52469a053c9c38354fbd6856a8a19f10bb84646
dmitriano <winappdev@gmail.com>
2020-12-19 04:14:05 +0300

    Separator

---
+++ Awl/Separator.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "Awl/String.h"
+
+namespace awl
+{
+    template <class C>
+    class basic_separator
+    {
+    public:
+
+        basic_separator(C ch) : m_ch(ch)
+        {
+        }
+
+        bool first() const
+        {
+            return m_first;
+        }
+
+        void hlop()
+        {
+            m_first = false;
+        }
+
+        C content() const
+        {
+            return m_ch;
+        }
+
+    private:
+
+        C m_ch;
+        
+        bool m_first = true;
+    };
+
+    template <class C>
+    inline std::basic_ostream<C>& operator << (std::basic_ostream<C>& out, basic_separator<C>& sep)
+    {
+        if (sep.first())
+        {
+            sep.hlop();
+        }
+        else
+        {
+            out << ' ' << sep.content();
+        }
+        
+        return out;
+    }
+
+    using separator = basic_separator<Char>;
+    using aseparator = basic_separator<char>;
+    using wseparator = basic_separator<wchar_t>;
+}
---
+++ Tests/SeparatorTest.cpp
@@ -0,0 +1,30 @@
+#include "Awl/Separator.h"
+#include "Awl/Testing/UnitTest.h"
+
+namespace SeparatorTest
+{
+    template <class C>
+    void Test()
+    {
+        //Why isn't it required?
+        //using awl::operator<<;
+        
+        awl::basic_separator<C> sep(',');
+        
+        std::basic_ostringstream<C> out;
+
+        out << sep << sep << sep;
+
+        auto result = out.str();
+        
+        AWT_ASSERT(result == awl::StringConvertor<C>::Decode(" , ,"));
+    }
+}
+
+AWT_TEST(Separator)
+{
+    AWT_UNUSED_CONTEXT;
+
+    SeparatorTest::Test<char>();
+    SeparatorTest::Test<wchar_t>();
+}
