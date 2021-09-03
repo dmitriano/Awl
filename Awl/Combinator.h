65576c37e090c21f99300f0651296cf4abf2aa05
dmitriano <winappdev@gmail.com>
2021-09-03 14:23:59 +0300

    added y_combinator

---
+++ Awl/Combinator.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <functional>
+
+namespace awl
+{
+    template<class R, class...Args>
+    auto y_combinator = [](auto f)
+    {
+        auto action = [=](auto action) -> std::function<R(Args...)>
+        {
+            return [=](Args&&... args)->R
+            {
+                return f(action(action), std::forward<Args>(args)...);
+            };
+        };
+        
+        return action(action);
+    };
+}
