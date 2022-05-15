c129d28d7df31ce5e94706c6fb9addb8fc4c382f
dmitriano <winappdev@gmail.com>
2022-05-15 20:24:18 +0300

    cc

--- Tests/CoroTimerTest.cpp
+++ Tests/CoroTimerTest.cpp
@@ -8,42 +8,37 @@
 #include "Awl/Testing/UnitTest.h"
 #include "Awl/Testing/TimeQueue.h"
 
-#include <coroutine>
-#include <optional>
-#include <iostream>
-#include <chrono>
-
 namespace
 {
     awl::testing::TimeQueue time_queue;
 
     using namespace std::chrono_literals;
 
-    awl::ProcessTask<int> wait_n(int n)
+    awl::ProcessTask<int> wait_n(const awl::testing::TestContext& context, int n)
     {
-        std::cout << "before wait " << n << '\n';
+        context.out << "before wait " << n << '\n';
         co_await awl::testing::TimeAwaitable(time_queue, std::chrono::seconds(n));
-        std::cout << "after wait " << n << '\n';
+        context.out << "after wait " << n << '\n';
         co_return n;
     }
 
-    awl::ProcessTask<int> test()
+    awl::ProcessTask<int> test(const awl::testing::TestContext& context)
     {
         for (auto c : "hello world\n")
         {
-            std::cout << c;
+            context.out << c;
             co_await awl::testing::TimeAwaitable(time_queue, 100ms);
         }
 
-        std::cout << "test step 1\n";
-        auto w3 = wait_n(3);
-        std::cout << "test step 2\n";
-        auto w2 = wait_n(2);
-        std::cout << "test step 3\n";
-        auto w1 = wait_n(1);
-        std::cout << "test step 4\n";
+        context.out << "test step 1\n";
+        auto w3 = wait_n(context, 3);
+        context.out << "test step 2\n";
+        auto w2 = wait_n(context, 2);
+        context.out << "test step 3\n";
+        auto w1 = wait_n(context, 1);
+        context.out << "test step 4\n";
         auto r = co_await w2 + co_await w3;
-        std::cout << "awaiting already computed coroutine\n";
+        context.out << "awaiting already computed coroutine\n";
         co_return co_await w1 + r;
     }
 }
@@ -51,7 +46,7 @@ namespace
 // main can't be a coroutine and usually need some sort of looper (io_service or timer loop in this example)
 AWT_EXAMPLE(CoroTimer)
 {
-    auto result = test();
+    auto result = test(context);
 
     // execute deferred coroutines
     time_queue.loop();
