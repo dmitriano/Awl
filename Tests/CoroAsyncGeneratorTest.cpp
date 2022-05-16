4cbb15084153a96c52701cde215a1c402af27fba
dmitriano <winappdev@gmail.com>
2022-05-16 03:59:17 +0300

    cc

--- Tests/CoroAsyncGeneratorTest.cpp
+++ Tests/CoroAsyncGeneratorTest.cpp
@@ -28,31 +28,27 @@ namespace
 
     awl::UpdateTask print(const awl::testing::TestContext& context)
     {
-        //There is no 'for co_await' yet in C++20.
-        //for co_await(int i : gen())
-
         awl::separator sep(_T(','));
         
+        //Unfortunately, 'for co_await' syntax is not approved for C++20 (I hope for now!) and instead of an elegant code we have to write
+        //old school for loop with previously captured by rvalue generator.
+        //for co_await(int i : gen())
+
         auto g = gen();
 
-        auto i = co_await g.begin();
-
-        while (i != g.end())
+        for (auto i = co_await g.begin(); i != g.end(); co_await ++i)
         {
             context.out << sep << *i;
-
-            co_await ++i;
+            context.out.flush();
         }
 
         context.out << std::endl;
     }
 }
 
-// main can't be a coroutine and usually need some sort of looper (io_service or timer loop in this example)
 AWT_EXAMPLE(CoroAsyncGenerator)
 {
     awl::UpdateTask task = print(context);
 
-    // execute deferred coroutines
     time_queue.loop();
 }
