91131f11e493ba7f987083ffea0111ba325f72fb
dmitriano <winappdev@gmail.com>
2022-06-03 13:07:46 +0300

    cc

---
+++ Extras/Boost/BoostExtras/MultiprecisionTraits.h
@@ -0,0 +1,52 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Product: AWL (A Working Library)
+// Author: Dmitriano
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+namespace awl
+{
+    namespace detail
+    {
+        template <class T>
+        struct multiprecision_descriptor;
+
+        template <>
+        struct multiprecision_descriptor<boost::multiprecision::uint128_t>
+        {
+            using signed_type = boost::multiprecision::int128_t;
+
+            static constexpr std::size_t size = 16;
+        };
+
+        template <>
+        struct multiprecision_descriptor<boost::multiprecision::uint256_t>
+        {
+            using signed_type = boost::multiprecision::int256_t;
+
+            static constexpr std::size_t size = 32;
+        };
+
+        template <>
+        struct multiprecision_descriptor<boost::multiprecision::uint512_t>
+        {
+            using signed_type = boost::multiprecision::int512_t;
+
+            static constexpr std::size_t size = 64;
+        };
+
+        template <>
+        struct multiprecision_descriptor<boost::multiprecision::uint1024_t>
+        {
+            using signed_type = boost::multiprecision::int1024_t;
+
+            static constexpr std::size_t size = 128;
+        };
+
+        template <class T>
+        using make_signed_t = typename multiprecision_descriptor<T>::signed_type;
+    }
+}
