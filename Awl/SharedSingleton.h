34c4748f24370a357914ac8e3243dd8f9a7c0016
Dmitriano <winappdev@gmail.com>
2022-10-06 17:34:02 +0300

    SharedSingleton test

--- Awl/SharedSingleton.h
+++ Awl/SharedSingleton.h
@@ -10,8 +10,45 @@
 
 namespace awl
 {
+    template <class T>
+    bool is_uninitialized(const std::weak_ptr<T>& wp)
+    {
+        using WeakPtr = std::weak_ptr<T>;
+        return !wp.owner_before(WeakPtr{}) && !WeakPtr{}.owner_before(wp);
+    }
+
+    //Let the user define a function that crates its singleton instance.
+    template <class T>
+    std::shared_ptr<T> make_singleton_instance();
+
     template <class T> requires std::is_default_constructible_v<T>
+    std::shared_ptr<T> make_singleton_instance()
+    {
+        //We have a long leaving std::weak_ptr so we may not use std::make_shared.
+        return std::make_shared<T>();
+    }
+
+    //Does not recreate the instance after it was destroyed, but returns nullptr.
+    template <class T>
     std::shared_ptr<T> shared_singleton()
+    {
+        static std::weak_ptr<T> wp;
+
+        if (is_uninitialized(wp))
+        {
+            std::shared_ptr<T> p = make_singleton_instance<T>();
+
+            wp = p;
+
+            return p;
+        }
+
+        return wp.lock();
+    }
+
+    //Alternative implementation that recreates the instance.
+    template <class T>
+    std::shared_ptr<T> ondemand_singleton()
     {
         static std::weak_ptr<T> wp;
 
@@ -19,7 +56,8 @@ namespace awl
 
         if (p == nullptr)
         {
-            p = std::make_shared<T>();
+            //We have a long leaving std::weak_ptr so we may not use std::make_shared.
+            p = make_singleton_instance<T>();
 
             wp = p;
         }
--- Tests/SingletonTest.cpp
+++ Tests/SingletonTest.cpp
@@ -5,6 +5,7 @@
 
 #include "Experimental/Destructible.h"
 #include "Awl/ScopeGuard.h"
+#include "Awl/SharedSingleton.h"
 #include "Awl/Testing/UnitTest.h"
 
 #include "Helpers/NonCopyable.h"
@@ -14,41 +15,22 @@
 namespace
 {
     constexpr int value = 5;
-    
+
     using A = awl::testing::helpers::NonCopyable;
+}
 
-    template <typename T>
-    bool is_uninitialized(std::weak_ptr<T> const& weak)
+namespace awl
+{
+    template <>
+    std::shared_ptr<A> make_singleton_instance<>()
     {
-        using wt = std::weak_ptr<T>;
-        return !weak.owner_before(wt{}) && !wt{}.owner_before(weak);
+        return std::make_shared<A>(value);
     }
-
-    template <class T>
-    std::shared_ptr<T> CreateObject();
 
     template <>
-    std::shared_ptr<A> CreateObject()
+    std::shared_ptr<int> make_singleton_instance<>()
     {
-        //We have a long leaving std::weak_ptr so we do not use std::make_shared.
-        return std::shared_ptr<A>(new A(value));
-    }
-
-    template <class T>
-    std::shared_ptr<T> GetObject()
-    {
-        static std::weak_ptr<T> wp;
-
-        if (is_uninitialized(wp))
-        {
-            std::shared_ptr<T> p = CreateObject<T>();
-
-            wp = p;
-
-            return p;
-        }
-
-        return wp.lock();
+        return std::make_shared<int>(23);
     }
 }
 
@@ -76,13 +58,13 @@ AWT_TEST(SharedSingleton)
     AWT_ASSERT_EQUAL(0, A::count);
 
     {
-        auto p1 = GetObject<A>();
+        auto p1 = awl::shared_singleton<A>();
 
         AWT_ASSERT_EQUAL(1, A::count);
         AWT_ASSERT(*p1 == A(value));
 
         {
-            auto p2 = GetObject<A>();
+            auto p2 = awl::shared_singleton<A>();
 
             AWT_ASSERT_EQUAL(1, A::count);
             AWT_ASSERT(*p2 == A(value));
@@ -92,6 +74,49 @@ AWT_TEST(SharedSingleton)
     }
 
     AWT_ASSERT_EQUAL(0, A::count);
-    AWT_ASSERT(!GetObject<A>());
+    AWT_ASSERT(!awl::shared_singleton<A>());
     AWT_ASSERT_EQUAL(0, A::count);
+}
+
+AWT_TEST(OnDemandSingleton)
+{
+    AWT_UNUSED_CONTEXT;
+
+    AWT_ASSERT_EQUAL(0, A::count);
+
+    {
+        auto p1 = awl::ondemand_singleton<A>();
+
+        AWT_ASSERT_EQUAL(1, A::count);
+        AWT_ASSERT(*p1 == A(value));
+
+        {
+            auto p2 = awl::ondemand_singleton<A>();
+
+            AWT_ASSERT_EQUAL(1, A::count);
+            AWT_ASSERT(*p2 == A(value));
+        }
+
+        AWT_ASSERT_EQUAL(1, A::count);
+    }
+
+    AWT_ASSERT_EQUAL(0, A::count);
+    AWT_ASSERT(awl::ondemand_singleton<A>() != nullptr);
+    AWT_ASSERT_EQUAL(0, A::count);
+
+    {
+        auto p1 = awl::ondemand_singleton<A>();
+
+        AWT_ASSERT_EQUAL(1, A::count);
+        AWT_ASSERT(*p1 == A(value));
+
+        {
+            auto p2 = awl::ondemand_singleton<A>();
+
+            AWT_ASSERT_EQUAL(1, A::count);
+            AWT_ASSERT(*p2 == A(value));
+        }
+
+        AWT_ASSERT_EQUAL(1, A::count);
+    }
 }
