a44cfe3acdc2f03069d92d2d7f66fc09f7824cf7
Dmitriano <winappdev@gmail.com>
2022-11-22 06:23:20 +0300

    cc

--- Awl/CppStd/StringView.h
+++ Awl/CppStd/StringView.h
@@ -9,7 +9,7 @@
         template<class C, class It, class End >
         constexpr std::basic_string_view<C> make_string_view(It begin, End end)
         {
-            const C* s = &(*begin);
+            const C* s = std::addressof(*begin);
             typename std::basic_string_view<C>::size_type count = static_cast<typename std::basic_string_view<C>::size_type>(end - begin);
 
             return std::basic_string_view<C>(s, count);
