f3d45c5cc2165e36f339295a6a10cffab935ab8c
Dmitriano <winappdev@gmail.com>
2024-11-08 14:39:05 +0300

    updated type_descriptor for boost::multiprecision

--- Extras/Boost/BoostExtras/Io/MultiprecisionTypeName.h
+++ Extras/Boost/BoostExtras/Io/MultiprecisionTypeName.h
@@ -5,14 +5,13 @@
 
 #pragma once
 
-#include "Awl/Io/TypeName.h"
+#include "Awl/Mp/TypeDescriptor.h"
 
 #include <boost/multiprecision/cpp_int.hpp>
 
-namespace awl::io
+namespace awl::mp
 {
-    template <class T>
-    struct type_descriptor<T, std::enable_if_t<
+    template <class T> requires (
         std::is_same_v<T, boost::multiprecision::int128_t> ||
         std::is_same_v<T, boost::multiprecision::uint128_t> ||
         std::is_same_v<T, boost::multiprecision::int256_t> ||
@@ -20,8 +19,12 @@ namespace awl::io
         std::is_same_v<T, boost::multiprecision::int512_t> ||
         std::is_same_v<T, boost::multiprecision::uint512_t> ||
         std::is_same_v<T, boost::multiprecision::int1024_t> ||
-        std::is_same_v<T, boost::multiprecision::uint1024_t>>>
+        std::is_same_v<T, boost::multiprecision::uint1024_t >
+    )
+    struct type_descriptor<T>
     {
+        using inner_tuple = std::tuple<>;
+
         static constexpr auto name()
         {
             return fixed_string("multi_int");
