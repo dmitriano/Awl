f5facf8b21cb5ea6155d03473907ac6441042472
Dmitriano <winappdev@gmail.com>
2025-02-03 14:26:12 +0300

    got rid of type

--- Awl/Testing/LocalAttribute.h
+++ Awl/Testing/LocalAttribute.h
@@ -10,10 +10,8 @@
 namespace awl::testing
 {
     template <class T, class Provider>
-    T GetAttributeValue(Provider& provider, const Char* type, const Char* name, T default_val)
+    T GetAttributeValue(Provider& provider, const Char* name, T default_val)
     {
-        static_cast<void>(type);
-
         T val;
 
         if (!provider.TryGet(name, val))
@@ -26,6 +24,6 @@ namespace awl::testing
 }
 
 #define AWT_ATTRIBUTE(attribute_type, attribute_name, default_val) const attribute_type attribute_name(awl::testing::GetAttributeValue<attribute_type>( \
-    context.ap, _T(#attribute_type), _T(#attribute_name), default_val))
+    context.ap, _T(#attribute_name), default_val))
 
 #define AWT_FLAG(attribute_name) AWT_ATTRIBUTE(bool, attribute_name, false)
