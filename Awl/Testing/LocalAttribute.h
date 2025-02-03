d84500ad443af87d375638ce294ea08c97918d26
Dmitriano <winappdev@gmail.com>
2025-02-03 14:57:36 +0300

    AWT_VARIABLE_ATTRIBUTE

--- Awl/Testing/LocalAttribute.h
+++ Awl/Testing/LocalAttribute.h
@@ -25,7 +25,9 @@ namespace awl::testing
     }
 }
 
-#define AWT_ATTRIBUTE(attribute_type, attribute_name, default_val) const attribute_type attribute_name(awl::testing::GetAttributeValue<attribute_type>( \
+#define AWT_VARIABLE_ATTRIBUTE(attribute_type, attribute_name, default_val) attribute_type attribute_name(awl::testing::GetAttributeValue<attribute_type>( \
     context.ap, #attribute_name, default_val))
+
+#define AWT_ATTRIBUTE(attribute_type, attribute_name, default_val) const AWT_VARIABLE_ATTRIBUTE(attribute_type, attribute_name, default_val)
 
 #define AWT_FLAG(attribute_name) AWT_ATTRIBUTE(bool, attribute_name, false)
