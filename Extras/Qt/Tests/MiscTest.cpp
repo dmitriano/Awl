c195231367882911e6e294e2134bc7198c65b315
Dmitriano <winappdev@gmail.com>
2025-07-07 22:29:44 +0300

    198. House Robber

--- Extras/Qt/Tests/MiscTest.cpp
+++ Extras/Qt/Tests/MiscTest.cpp
@@ -5,17 +5,56 @@
  
 #include <Awl/Testing/UnitTest.h>
 
-#include <QString>
-#include <QDebug>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
 
-//Ensure we did not mix release/debug builds.
-AWL_TEST(QtMixReleaseDebug)
+class Solution
 {
-    //AWL_ATTRIBUTE(QString, qstr, QString("QT6"));
+public:
+
+    int rob(std::vector<int>& nums)
+    {
+        std::vector<int> sums;
+
+        sums.reserve(nums.size());
+
+        if (nums.size() >= 1u)
+        {
+            sums.push_back(nums[0]);
+        }
+        else
+        {
+            return 0;
+        }
+
+        if (nums.size() >= 2u)
+        {
+            sums.push_back(std::max(nums[0], nums[1]));
+        }
+
+        for (size_t n = 2; n < nums.size(); ++n)
+        {
+            int s1 = nums[n] + sums[n - 2];
+
+            int s2 = sums[n - 1];
+
+            sums.push_back(std::max(s1, s2));
+        }
+
+        return sums.back();
+    }
+};
+
+AWL_TEST(Interview)
+{
     AWL_UNUSED_CONTEXT;
 
-    const QString qstr = "QT6";
-    const std::string val = qstr.toStdString();
-    //context.out << awl::FromAString(val);
-    qDebug() << QString::fromStdString(val);
+    AWL_VARIABLE_ATTRIBUTE(std::vector<int>, nums, {});
+    AWL_ATTRIBUTE(int, expected_sum, 0);
+
+    Solution sol;
+
+    AWL_ASSERT(sol.rob(nums) == expected_sum);
 }
