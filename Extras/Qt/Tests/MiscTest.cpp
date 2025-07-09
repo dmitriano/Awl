599bd35969ab80c12c1fabc9304eb8122fe5a93c
Dmitriano <winappdev@gmail.com>
2025-07-09 20:02:05 +0300

    Getter

--- Extras/Qt/Tests/MiscTest.cpp
+++ Extras/Qt/Tests/MiscTest.cpp
@@ -8,53 +8,75 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <unordered_set>
 #include <string>
 
-class Solution
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+struct Device
+{
+    std::string Version;
+    int InventoryNumber;
+    std::vector<std::shared_ptr<Device>> Children;
+};
+
+void f()
+{
+    auto getter = std::mem_fn(&Device::Version);
+
+    using Getter = decltype(getter);
+
+    using Field = std::decay_t<std::invoke_result_t<Getter, Device&>>;
+
+    static_assert(std::is_same_v<Field, std::string>);
+}
+
+template <class Getter>
+class Finder
 {
 public:
 
-    int rob(std::vector<int>& nums)
+    using Field = std::decay_t<std::invoke_result_t<Getter, Device&>>;
+
+    Finder(Getter func) : m_func(std::move(func)) {}
+
+    std::vector<Field> FindDistinctValues(std::shared_ptr<Device> rootDevice)
     {
-        std::vector<int> sums;
+        std::unordered_set<Field> set;
 
-        sums.reserve(nums.size());
+        FindDistinctValuesImpl(rootDevice, set);
 
-        if (nums.size() >= 1u)
-        {
-            sums.push_back(nums[0]);
-        }
-        else
-        {
-            return 0;
-        }
-
-        if (nums.size() >= 2u)
-        {
-            sums.push_back(std::max(nums[0], nums[1]));
-        }
-
-        for (size_t n = 2; n < nums.size(); ++n)
-        {
-            int s1 = nums[n] + sums[n - 2];
-
-            int s2 = sums[n - 1];
-
-            sums.push_back(std::max(s1, s2));
-        }
-
-        return sums.back();
+        return std::vector<Field>(std::make_move_iterator(set.begin()), std::make_move_iterator(set.end()));
     }
+
+private:
+
+    void FindDistinctValuesImpl(std::shared_ptr<Device> parentDevice, std::unordered_set<Field>& set)
+    {
+        Field val = m_func(parentDevice);
+
+        set.insert(val);
+
+        for (const std::shared_ptr<Device>& device : parentDevice->Children)
+        {
+            FindDistinctValuesImpl(device, set);
+        }
+    }
+
+    Getter m_func;
 };
 
 AWL_TEST(Interview)
 {
     AWL_UNUSED_CONTEXT;
 
-    AWL_VARIABLE_ATTRIBUTE(std::vector<int>, nums, {});
-    AWL_ATTRIBUTE(int, expected_sum, 0);
+    std::shared_ptr<Device> d = std::make_shared<Device>(Device{ "v1", 1, std::vector<std::shared_ptr<Device>>{
+        std::make_shared<Device>(Device{"v2", 2, std::vector<std::shared_ptr<Device>>{}})} });
 
-    Solution sol;
+    auto versions = Finder{ std::mem_fn(&Device::Version) }.FindDistinctValues(d);
 
-    AWL_ASSERT(sol.rob(nums) == expected_sum);
+    auto numbers = Finder{ std::mem_fn(&Device::InventoryNumber) }.FindDistinctValues(d);
 }
