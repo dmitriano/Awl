13b8cb18904e105c74a820b4194489841865bef4
Dmitriano <winappdev@gmail.com>
2025-10-06 18:45:22 +0300

    producer crashes

--- Extras/Boost/Tests/CobaltTest.cpp
+++ Extras/Boost/Tests/CobaltTest.cpp
@@ -13,50 +13,71 @@ namespace asio = boost::asio;
 namespace cobalt = boost::cobalt;
 using namespace std::chrono_literals;
 
-std::generator<int> numbers()
+namespace
 {
-    for (int i = 1; i <= 5; ++i)
+    cobalt::generator<int> number_source(int count)
     {
-        co_yield i;
-    }
-}
+        auto exec = co_await cobalt::this_coro::executor;
+        asio::steady_timer timer(exec);
 
-cobalt::generator<int> numbers2()
-{
-    for (int i = 1; i <= 5; ++i)
-    {
-        // co_await cobalt::this_coro::sleep_for(std::chrono::milliseconds(200));
-        
-        co_yield i;
-    }
+        for (int i = 1; i <= count; ++i)
+        {
+            timer.expires_after(500ms);
+            co_await timer.async_wait(cobalt::use_op);
+            co_yield i;
+        }
 
-    co_return 10;
-}
-
-cobalt::generator<int> numbers3(asio::any_io_executor exec)
-{
-    asio::steady_timer timer(exec);
-
-    for (int i = 1; i <= 5; ++i)
-    {
-        timer.expires_after(500ms);
-        co_await timer.async_wait(cobalt::use_op);
-        co_yield i;
+        co_return 0;
     }
 
-    co_return 0;
+    cobalt::task<void> producer(cobalt::channel<int>& ch)
+    {
+        auto gen = number_source(5);
+
+        BOOST_COBALT_FOR(auto val, gen)
+        {
+            std::cout << "Produced: " << val << "\n";
+            co_await ch.write(val);  // асинхронно отправляем значение в канал
+        }
+
+        ch.close();  // обязательно закрыть канал, чтобы читатели завершились
+        std::cout << "Producer finished\n";
+        co_return;
+    }
+
+    cobalt::task<void> consumer(int id, cobalt::channel<int>& ch)
+    {
+        auto exec = co_await cobalt::this_coro::executor;
+        asio::steady_timer timer(exec);
+
+        while (true)
+        {
+            auto val = co_await ch.read();  // ждём данных из канала
+            if (!val)
+                break;  // канал закрыт
+
+            std::cout << "Consumer " << id << " got: " << val << "\n";
+
+            timer.expires_after(500ms);
+            co_await timer.async_wait(cobalt::use_op);
+        }
+
+        std::cout << "Consumer " << id << " finished\n";
+
+        co_return;
+    }
 }
 
 cobalt::main co_main(int argc, char* argv[])
 {
+    cobalt::channel<int> ch{ 0u };
+
     auto exec = co_await cobalt::this_coro::executor;
 
-    auto gen = numbers3(exec);
-
-    BOOST_COBALT_FOR(auto val, gen)
-    {
-        std::cout << "Got value: " << val << std::endl;
-    }
+    // Запускаем продюсера и двух консюмеров параллельно
+    cobalt::spawn(exec, producer(ch), boost::asio::detached);
+    // cobalt::spawn(exec, consumer(1, ch), boost::asio::detached);
+    // cobalt::spawn(exec, consumer(2, ch), boost::asio::detached);
 
     co_return 0;
 }
