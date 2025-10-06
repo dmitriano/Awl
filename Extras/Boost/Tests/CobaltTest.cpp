9f28b842198f910af6f00ff4cbf34923798e1c6b
Dmitriano <winappdev@gmail.com>
2025-10-06 19:02:24 +0300

    2 iterating coroutines

--- Extras/Boost/Tests/CobaltTest.cpp
+++ Extras/Boost/Tests/CobaltTest.cpp
@@ -30,6 +30,8 @@ namespace
         co_return 0;
     }
 
+    using GenPtr = std::shared_ptr<cobalt::generator<int>>;
+
     cobalt::task<void> producer(cobalt::channel<int>& ch)
     {
         auto gen = number_source(5);
@@ -45,14 +47,14 @@ namespace
         co_return;
     }
 
-    cobalt::task<void> consumer(int id, cobalt::channel<int>& ch)
+    cobalt::task<void> consumer(int id, GenPtr gen)
     {
         auto exec = co_await cobalt::this_coro::executor;
         asio::steady_timer timer(exec);
 
         while (true)
         {
-            auto val = co_await ch.read();  // ждём данных из канала
+            auto val = co_await *gen;
             if (!val)
                 break;  // канал закрыт
 
@@ -70,14 +72,13 @@ namespace
 
 cobalt::main co_main(int argc, char* argv[])
 {
-    cobalt::channel<int> ch{ 0u };
-
+    GenPtr gen = std::make_shared<cobalt::generator<int>>(number_source(10));
+    
     auto exec = co_await cobalt::this_coro::executor;
 
-    // Запускаем продюсера и двух консюмеров параллельно
-    cobalt::spawn(exec, producer(ch), boost::asio::detached);
-    // cobalt::spawn(exec, consumer(1, ch), boost::asio::detached);
-    // cobalt::spawn(exec, consumer(2, ch), boost::asio::detached);
+    // cobalt::spawn(exec, producer(ch), boost::asio::detached);
+    cobalt::spawn(exec, consumer(1, gen), boost::asio::detached);
+    cobalt::spawn(exec, consumer(2, gen), boost::asio::detached);
 
     co_return 0;
 }
