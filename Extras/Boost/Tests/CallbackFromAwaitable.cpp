73a927a8d6c34b316c92a54f570a02b1f49be4af
Dmitriano <winappdev@gmail.com>
2025-10-28 17:00:12 +0300

    cc

--- Extras/Boost/Tests/CallbackFromAwaitable.cpp
+++ Extras/Boost/Tests/CallbackFromAwaitable.cpp
@@ -7,54 +7,57 @@
 namespace asio = boost::asio;
 using boost::system::error_code;
 
-// -------------------------------------------
-// Bridge: awaitable<T>  ---> async-style handler: void(error_code, T)
-// -------------------------------------------
-template <class T, class CompletionToken>
-auto async_from_awaitable(asio::any_io_executor ex,
-    asio::awaitable<T> aw,
-    CompletionToken&& token)
+namespace
 {
-    using token_type = std::decay_t<CompletionToken>;
+    // -------------------------------------------
+    // Bridge: awaitable<T>  ---> async-style handler: void(error_code, T)
+    // -------------------------------------------
+    template <class T, class CompletionToken>
+    auto async_from_awaitable(asio::any_io_executor ex,
+        asio::awaitable<T> aw,
+        CompletionToken&& token)
+    {
+        using token_type = std::decay_t<CompletionToken>;
 
-    // Materialize the token as an lvalue to satisfy async_initiate on MSVC
-    token_type tk(std::forward<CompletionToken>(token));
+        // Materialize the token as an lvalue to satisfy async_initiate on MSVC
+        token_type tk(std::forward<CompletionToken>(token));
 
-    return asio::async_initiate<token_type, void(error_code, T)>(
-        [ex, aw = std::move(aw)](auto&& handler) mutable {
-            // Spawn the coroutine; on completion, deliver via the handler
-            asio::co_spawn(
-                ex,
-                std::move(aw),
-                // co_spawn completion for non-void: void(std::exception_ptr, T)
-                [h = std::forward<decltype(handler)>(handler)]
-                (std::exception_ptr ep, T value) mutable
-                {
-                    if (ep) {
-                        try {
-                            std::rethrow_exception(ep);
+        return asio::async_initiate<token_type, void(error_code, T)>(
+            [ex, aw = std::move(aw)](auto&& handler) mutable {
+                // Spawn the coroutine; on completion, deliver via the handler
+                asio::co_spawn(
+                    ex,
+                    std::move(aw),
+                    // co_spawn completion for non-void: void(std::exception_ptr, T)
+                    [h = std::forward<decltype(handler)>(handler)]
+                    (std::exception_ptr ep, T value) mutable
+                    {
+                        if (ep) {
+                            try {
+                                std::rethrow_exception(ep);
+                            }
+                            catch (const boost::system::system_error& se) {
+                                std::move(h)(se.code(), T{});
+                                return;
+                            }
+                            catch (...) {
+                                std::move(h)(
+                                    make_error_code(boost::system::errc::operation_canceled), T{});
+                                return;
+                            }
                         }
-                        catch (const boost::system::system_error& se) {
-                            std::move(h)(se.code(), T{});
-                            return;
-                        }
-                        catch (...) {
-                            std::move(h)(
-                                make_error_code(boost::system::errc::operation_canceled), T{});
-                            return;
-                        }
+                        std::move(h)({}, std::move(value));
                     }
-                    std::move(h)({}, std::move(value));
-                }
-            );
-        },
-        tk // pass as lvalue
-    );
-}
+                );
+            },
+            tk // pass as lvalue
+        );
+    }
 
-asio::awaitable<int> compute_answer()
-{
-    co_return 42;
+    asio::awaitable<int> compute_answer()
+    {
+        co_return 42;
+    }
 }
 
 AWL_EXAMPLE(CallbackFromAwaitable)
