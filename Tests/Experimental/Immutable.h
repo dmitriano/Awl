98fe4e2e4d5776efcc14b33c80a3576feccbb3ed
Dmitriano <winappdev@gmail.com>
2025-11-07 17:12:05 +0300

    cc

--- Tests/Experimental/Immutable.h
+++ Tests/Experimental/Immutable.h
@@ -60,7 +60,7 @@ namespace awl
     private:
 
 
-// This can be done via a template paramter template<class T, like Checker = FakeChecker<T>>
+// This can be done via a template paramter like template<class T, Checker = FakeChecker<T>>
 #ifdef AWL_DEBUG_IMMUTABLE
         
         constexpr void markAsMoved()
