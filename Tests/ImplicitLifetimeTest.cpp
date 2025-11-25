418923dff38cc4a3226530117becfece632c6601
Dmitriano <winappdev@gmail.com>
2025-11-25 15:28:53 +0300

    class X

--- Tests/ImplicitLifetimeTest.cpp
+++ Tests/ImplicitLifetimeTest.cpp
@@ -11,149 +11,75 @@
 
 using namespace awl::testing;
 
-namespace
+class VirtualClass
 {
-    template<std::size_t N>
-    struct MyAllocator
-    {
-        MyAllocator()
-        {
-            for (std::byte& b : data)
-                b = std::byte{ 3 };
-        }
+public:
+    virtual ~VirtualClass() = default;
+};
 
-        std::byte data[N];
-        std::size_t sz{ N };
-        void* p{ data };
-
-        // Note: only well-defined for implicit-lifetime types
-        template<typename T>
-        T* implicit_aligned_alloc(std::size_t a = alignof(T))
-        {
-            if (std::align(a, sizeof(T), p, sz))
-            {
-                T* result1 = reinterpret_cast<T*>(p);
-                T* result2 = std::launder(result1);
-                p = static_cast<std::byte*>(p) + sizeof(T);
-                sz -= sizeof(T);
-                return result2;
-            }
-            return nullptr;
-        }
-    };
-
-    struct A { int x; };
-}
-
-AWL_EXAMPLE(ImplicitLifetimeAllocator)
+class VirtualClass2
 {
-    AWL_UNUSED_CONTEXT;
+public:
+    virtual ~VirtualClass2() = default;
+};
 
-    MyAllocator<64> a;
+class DerivedVirtualClass : public VirtualClass
+{
+public:
+    virtual ~DerivedVirtualClass() = default;
+};
 
-    std::cout << "allocated a.data at " << (void*)a.data
-        << " (" << sizeof a.data << " bytes)\n";
+class Derived2VirtualClass
+    : public VirtualClass
+    , public VirtualClass2
+{
+public:
+    virtual ~Derived2VirtualClass() = default;
+};
 
-    // Allocate A
-    if (A* p = a.implicit_aligned_alloc<A>())
-    {
-        std::cout << "allocated A char at " << (void*)p << '\n';
-        std::cout << "\tOriginal value: " << std::format("{:x}", p->x) << '\n';
-        *p = {};
-        std::cout << "\tDefault value: " << std::format("{:x}", p->x) << '\n';
-        p->x = 0x42;
-        std::cout << "\tUpdated value: " << std::format("{:x}", p->x) << '\n';
-    }
-
-    // Allocate a char
-    if (char* p = a.implicit_aligned_alloc<char>())
-    {
-        *p = 'a';
-        std::cout << "allocated a char at " << (void*)p << '\n';
-    }
-
-    // Allocate an int
-    if (int* p = a.implicit_aligned_alloc<int>())
-    {
-        *p = 1;
-        std::cout << "allocated an int at " << (void*)p << '\n';
-    }
-
-    // Allocate an int, aligned at a 32-byte boundary
-    if (int* p = a.implicit_aligned_alloc<int>(32))
-    {
-        *p = 2;
-        std::cout << "allocated an int at " << (void*)p << " (32-byte alignment)\n";
-    }
-}
+template <class T>
+concept IsInterface =
+std::is_class_v<T> && not std::is_final_v<T> && std::has_virtual_destructor_v<T>
+&& (sizeof(T) == sizeof(VirtualClass) || sizeof(T) == sizeof(DerivedVirtualClass)
+    || sizeof(T) == sizeof(Derived2VirtualClass));
 
 namespace
 {
-    struct B
+    class A
     {
-        int x;
-        char y;
+    public:
 
-        int64_t z[10];
-
-        char y1;
+        virtual ~A() = default;
     };
+
+    class B : public A {};
+
+    class C : public A {};
+
+    class D : public B, public C {};
+
+    class E : public B, public C {};
+
+    class F : public D, public E {};
+
+    class A1
+    {
+    public:
+
+        virtual ~A1() = default;
+    };
+
+    class X : public F, public A1 {};
+
+    // static_assert(IsInterface<D>);
 }
 
-AWL_EXAMPLE(ImplicitLifetimeVector)
+AWL_EXAMPLE(Interface)
 {
-    AWL_ATTRIBUTE(size_t, offset, 0);
-
-    context.logger.debug(awl::format() << "sizeof(B): " << sizeof(B));
-
-    std::vector<uint8_t> v(sizeof(B) + offset, 0);
-
-    B* p = reinterpret_cast<B*>(v.data() + offset);
-
-    p->x = 1;
-    p->y = 'a';
-
-    context.logger.debug(awl::format() << p->x << " " << p->y << " " << v[offset + 4]);
-
-    v[offset + 0] = 10;
-    v[offset + 4] = 66;
-
-    context.logger.debug(awl::format() << p->x << " " << p->y);
-}
-
-AWL_EXAMPLE(ImplicitLifetimeCalloc)
-{
-    AWL_ATTRIBUTE(size_t, offset, 0);
-
-    context.logger.debug(awl::format() << "sizeof(B): " << sizeof(B) << ", alignof(B): " << alignof(B));
-
-    void* p1 = calloc(1, sizeof(B) + offset);
-
-    B* p = reinterpret_cast<B*>(reinterpret_cast<char*>(p1) + offset);
-
-    p->x = 1;
-    p->y = 'a';
-
-    context.logger.debug(awl::format() << p->x << " " << p->y);
-
-    free(p1);
-}
-
-AWL_EXAMPLE(ImplicitLifetimePlacementNew)
-{
-    struct S { int x; char c; };
-
-    alignas(S) char buf[sizeof(S)];
-    S* p = new(buf) S{};
-
-    p->x = 10;
-    p->c = 'a';
-
-    context.logger.debug(awl::format() << p->x << " " << buf[4]);
-
-    // An object of type char, unsigned char, or std::byte may be used to access the object representation of any object.
-    // A char, unsigned char, or std::byte array may be used to manipulate the object representation of any object.
-    buf[4] = 'b';
-
-    context.logger.debug(awl::format() << p->x << " " << p->c);
+    std::cout << "sizeof(A): " << sizeof(A) << std::endl;
+    std::cout << "sizeof(B): " << sizeof(B) << std::endl;
+    std::cout << "sizeof(C): " << sizeof(C) << std::endl;
+    std::cout << "sizeof(D): " << sizeof(D) << std::endl;
+    std::cout << "sizeof(E): " << sizeof(E) << std::endl;
+    std::cout << "sizeof(X): " << sizeof(E) << std::endl;
 }
