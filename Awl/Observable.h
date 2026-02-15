7ef7c8b3311e0e6cf1f273b8b6116b377de59077
Dmitriano <winappdev@gmail.com>
2026-02-15 18:03:09 +0300

    Observer specialization with std::function.

--- Awl/Observable.h
+++ Awl/Observable.h
@@ -7,6 +7,7 @@
 
 #include "Awl/Observer.h"
 
+#include <concepts>
 #include <functional>
 #include <type_traits>
 #include <utility>
@@ -79,23 +80,38 @@ namespace awl
 
             template <class Callable>
             void notifyImpl(Callable&& call)
+                requires std::invocable<Callable&, ObserverElement*>
             {
-                for (typename ObserverList::iterator i = m_observers.begin(); i != m_observers.end(); )
+                notifyLoopImpl([&](ObserverElement* p_observer)
                 {
-                    //p_observer can delete itself or unsubscribe while iterating over the list so we use postfix ++
-                    IObserver* p_observer = *(i++);
-
                     call(p_observer);
-                }
+                    return true;
+                });
             }
 
             template <class Callable>
             bool notifyWhileTrueImpl(Callable&& call)
+                requires std::invocable<Callable&, ObserverElement*>
+            {
+                return notifyLoopImpl([&](ObserverElement* p_observer)
+                {
+                    return static_cast<bool>(call(p_observer));
+                });
+            }
+
+        private:
+
+            template <class Callable>
+            bool notifyLoopImpl(Callable&& call)
+                requires (
+                    std::invocable<Callable&, ObserverElement*> &&
+                    std::convertible_to<std::invoke_result_t<Callable&, ObserverElement*>, bool>
+                )
             {
                 for (typename ObserverList::iterator i = m_observers.begin(); i != m_observers.end(); )
                 {
                     //p_observer can delete itself or unsubscribe while iterating over the list so we use postfix ++
-                    IObserver* p_observer = *(i++);
+                    ObserverElement* p_observer = *(i++);
 
                     if (!static_cast<bool>(call(p_observer)))
                     {
@@ -105,8 +121,6 @@ namespace awl
 
                 return true;
             }
-
-        private:
 
             //If the observable is deleted before its observers,
             //we remove them from the list, otherwise they will think that they are included and
@@ -130,6 +144,7 @@ namespace awl
     private:
 
         using Base = details::ObservableImpl<IObserver, Enclosing>;
+        using ObserverElement = typename Base::ObserverElement;
 
     public:
 
@@ -153,9 +168,9 @@ namespace awl
         //compiler errors if Args does not match Params.
         template<typename ...Params, typename ... Args>
         void notify(void (IObserver::*func)(Params ...), const Args& ... args)
-            requires (sizeof...(Params) == sizeof...(Args) && (std::is_convertible_v<Args, Params> && ...))
+            requires (std::invocable<decltype(func), IObserver*, const Args&...>)
         {
-            Base::notifyImpl([&](IObserver* p_observer) { (p_observer->*func)(args ...); });
+            Base::notifyImpl([&](ObserverElement* p_observer) { (static_cast<IObserver*>(p_observer)->*func)(args ...); });
         }
 
         // It is not clear enough if we really need const notify methods like this:
@@ -166,11 +181,10 @@ namespace awl
         bool notifyWhileTrue(Result (IObserver::* func)(Params ...), const Args& ... args)
             requires (
                 std::is_convertible_v<Result, bool> &&
-                sizeof...(Params) == sizeof...(Args) &&
-                (std::is_convertible_v<Args, Params> && ...)
+                std::invocable<decltype(func), IObserver*, const Args&...>
             )
         {
-            return Base::notifyWhileTrueImpl([&](IObserver* p_observer) { return (p_observer->*func)(args ...); });
+            return Base::notifyWhileTrueImpl([&](ObserverElement* p_observer) { return (static_cast<IObserver*>(p_observer)->*func)(args ...); });
         }
 
         friend Enclosing;
@@ -182,8 +196,9 @@ namespace awl
     {
     private:
 
-        using Base = details::ObservableImpl<std::function<Result(Params...)>, Enclosing>;
-        using FunctionObserver = std::function<Result(Params...)>;
+        using Function = std::function<Result(Params...)>;
+        using Base = details::ObservableImpl<Function, Enclosing>;
+        using ObserverElement = typename Base::ObserverElement;
 
     public:
 
@@ -205,23 +220,19 @@ namespace awl
 
         template<typename ... Args>
         void notify(const Args& ... args)
-            requires (
-                sizeof...(Params) == sizeof...(Args) &&
-                (std::is_convertible_v<Args, Params> && ...)
-            )
+            requires (std::invocable<Function&, const Args&...>)
         {
-            Base::notifyImpl([&](FunctionObserver* p_observer) { (*p_observer)(args ...); });
+            Base::notifyImpl([&](ObserverElement* p_observer) { p_observer->function()(args ...); });
         }
 
         template<typename ... Args>
         bool notifyWhileTrue(const Args& ... args)
             requires (
                 std::is_convertible_v<Result, bool> &&
-                sizeof...(Params) == sizeof...(Args) &&
-                (std::is_convertible_v<Args, Params> && ...)
+                std::invocable<Function&, const Args&...>
             )
         {
-            return Base::notifyWhileTrueImpl([&](FunctionObserver* p_observer) { return (*p_observer)(args ...); });
+            return Base::notifyWhileTrueImpl([&](ObserverElement* p_observer) { return p_observer->function()(args ...); });
         }
 
         friend Enclosing;
--- Awl/Observer.h
+++ Awl/Observer.h
@@ -7,6 +7,9 @@
 
 #include "Awl/QuickList.h"
 
+#include <functional>
+#include <utility>
+
 namespace awl
 {
     AWL_DECLARE_QUICK_LINK(observer_link)
@@ -45,5 +48,62 @@ namespace awl
                 unsubscribeSelf();
             }
         }
+    };
+
+    template <class Result, class... Args>
+    class Observer<std::function<Result(Args...)>> : public observer_link
+    {
+    public:
+
+        using Function = std::function<Result(Args...)>;
+
+        Observer() = default;
+
+        explicit Observer(Function function) :
+            m_function(std::move(function))
+        {
+        }
+
+        Observer(const Observer& other) = delete;
+
+        Observer(Observer&& other) = default;
+
+        Observer& operator = (const Observer& other) = delete;
+
+        Observer& operator = (Observer&& other) = default;
+
+        ~Observer() = default;
+
+        bool isSubscribed() const
+        {
+            return observer_link::included();
+        }
+
+        void unsubscribeSelf()
+        {
+            observer_link::exclude();
+        }
+
+        void unsubscribeSafe()
+        {
+            if (isSubscribed())
+            {
+                unsubscribeSelf();
+            }
+        }
+
+        const Function& function() const
+        {
+            return m_function;
+        }
+
+        void setFunction(Function function)
+        {
+            m_function = std::move(function);
+        }
+
+    private:
+
+        Function m_function;
     };
 }
