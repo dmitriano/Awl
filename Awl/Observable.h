89f98e8ef41d2f9daa0636ac74f2f48efb2badc8
Dmitriano <winappdev@gmail.com>
2026-02-15 15:01:17 +0300

    gen

--- Awl/Observable.h
+++ Awl/Observable.h
@@ -7,6 +7,9 @@
 
 #include "Awl/Observer.h"
 
+#include <functional>
+#include <type_traits>
+
 namespace awl
 {
     template <class IObserver, class Enclosing = void>
@@ -109,6 +112,108 @@ namespace awl
             }
         }
         
+        using ObserverList = quick_list<ObserverElement, observer_link>;
+
+        ObserverList m_observers;
+
+        friend Enclosing;
+    };
+
+    template <class TResult, class... Params, class Enclosing>
+    class Observable<std::function<TResult(Params...)>, Enclosing>
+    {
+    private:
+
+        using FunctionObserver = std::function<TResult(Params...)>;
+        using ObserverElement = Observer<FunctionObserver>;
+
+    public:
+
+        Observable() = default;
+
+        ~Observable()
+        {
+            clearObservers();
+        }
+
+        Observable(const Observable& other) = delete;
+
+        Observable(Observable&& other) : m_observers(std::move(other.m_observers)) {}
+
+        Observable& operator = (const Observable& other) = delete;
+
+        Observable& operator = (Observable&& other) noexcept
+        {
+            clearObservers();
+            m_observers = std::move(other.m_observers);
+            return *this;
+        }
+
+        void subscribe(ObserverElement* p_observer)
+        {
+            m_observers.push_back(p_observer);
+        }
+
+        void unsubscribe(ObserverElement* p_observer)
+        {
+            p_observer->unsubscribeSelf();
+        }
+
+        bool empty() const
+        {
+            return m_observers.empty();
+        }
+
+        auto size() const
+        {
+            return m_observers.size();
+        }
+
+    protected:
+
+        void notify(Params ... args)
+        {
+            for (typename ObserverList::iterator i = m_observers.begin(); i != m_observers.end(); )
+            {
+                //p_observer can delete itself or unsubscribe while iterating over the list so we use postfix ++
+                FunctionObserver* p_observer = *(i++);
+
+                (*p_observer)(args ...);
+            }
+        }
+
+        bool notifyWhileTrue(Params ... args)
+        {
+            static_assert(std::is_convertible_v<TResult, bool>, "notifyWhileTrue requires std::function with bool-convertible result type.");
+
+            for (typename ObserverList::iterator i = m_observers.begin(); i != m_observers.end(); )
+            {
+                //p_observer can delete itself or unsubscribe while iterating over the list so we use postfix ++
+                FunctionObserver* p_observer = *(i++);
+
+                if (!static_cast<bool>((*p_observer)(args ...)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    private:
+
+        //If the observable is deleted before its observers,
+        //we remove them from the list, otherwise they will think that they are included and
+        //their destructors will delete them from already destroyed list.
+        //So we can't use m_observers.clear() here because it only clears list's head.
+        void clearObservers()
+        {
+            while (!m_observers.empty())
+            {
+                m_observers.pop_front();
+            }
+        }
+
         using ObserverList = quick_list<ObserverElement, observer_link>;
 
         ObserverList m_observers;
