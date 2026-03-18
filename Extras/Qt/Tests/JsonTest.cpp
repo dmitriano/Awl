f3f4ac25647a17904b8a179dd0a7295595613aa3
Dmitriano <winappdev@gmail.com>
2026-03-18 19:28:41 +0300

    zeros test

--- Extras/Qt/Tests/JsonTest.cpp
+++ Extras/Qt/Tests/JsonTest.cpp
@@ -379,10 +379,10 @@ AWL_TEST(JsonDuration)
         },
         std::array<QString, 4>
         {
-            "48:02:03.004",
-            "00:00:00",
-            "-48:02:03.004",
-            "48:02:03"
+            "48:02:03.004000",
+            "00:00:00.0000",
+            "-48:02:03.004000",
+            "48:02:03.000"
         });
 
     test_roundtrip.operator()<microseconds>("microseconds",
@@ -401,8 +401,8 @@ AWL_TEST(JsonDuration)
         std::array<QString, 3>
         {
             "48:02:03.456789",
-            "48:02:03.456",
-            "-48:02:03.456"
+            "48:02:03.4560000",
+            "-48:02:03.4560000"
         });
 
     test_roundtrip.operator()<seconds>("seconds",
@@ -420,9 +420,9 @@ AWL_TEST(JsonDuration)
         },
         std::array<QString, 3>
         {
-            "48:02:03",
-            "00:00:00",
-            "-48:02:03"
+            "48:02:03.000",
+            "00:00:00.0000",
+            "-48:02:03.000"
         });
 
     test_roundtrip.operator()<minutes>("minutes",
@@ -440,9 +440,9 @@ AWL_TEST(JsonDuration)
         },
         std::array<QString, 3>
         {
-            "48:02:00",
-            "00:00:00",
-            "-48:02:00"
+            "48:02:00.000",
+            "00:00:00.0000",
+            "-48:02:00.000"
         });
 
     test_roundtrip.operator()<hours>("hours",
@@ -460,9 +460,9 @@ AWL_TEST(JsonDuration)
         },
         std::array<QString, 3>
         {
-            "48:00:00",
-            "00:00:00",
-            "-48:00:00"
+            "48:00:00.000",
+            "00:00:00.0000",
+            "-48:00:00.000"
         });
 
     using system_duration = system_clock::duration;
@@ -485,9 +485,9 @@ AWL_TEST(JsonDuration)
         },
         std::array<QString, 3>
         {
-            "48:02:03",
-            "00:00:00",
-            "-48:02:03"
+            "48:02:03.0000000",
+            "00:00:00.0000000",
+            "-48:02:03.0000000"
         });
 }
 
