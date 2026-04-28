ce67cebde56e76879768d88e4cce6e93a744d322
Dmitriano <windappdev@gmail.com>
2026-04-28 14:07:50 +0300

    EnumFormatter.h

---
+++ Awl/EnumFormatter.h
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Product: AWL (A Working Library)
+// Author: Dmitriano
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Awl/EnumTraits.h"
+#include "Awl/StringFormat.h"
+
+#include <format>
+#include <string>
+
+namespace std
+{
+    template <awl::is_sequential_enum T, class CharT>
+    struct formatter<T, CharT> : formatter<std::string, CharT>
+    {
+        template <class FormatContext>
+        auto format(T val, FormatContext& ctx) const
+        {
+            return formatter<std::string, CharT>::format(awl::enum_to_string(val), ctx);
+        }
+    };
+}
