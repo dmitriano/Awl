c76503a5dd44e69c387ff420fb4360e0cb3cb049
Dmitriano <windappdev@gmail.com>
2026-04-29 14:05:28 +0300

    - ObservableImpl.h

--- Awl/Observable.h
+++ Awl/Observable.h
@@ -5,33 +5,63 @@
 
 #pragma once
 
-#include "Awl/ObservableImpl.h"
+#include "Awl/Observer.h"
+
+#include <concepts>
+#include <type_traits>
+#include <utility>
 
 namespace awl
 {
     template <class IObserver, class Enclosing = void>
-    class Observable : private details::ObservableImpl<IObserver, Enclosing>
+    class Observable
     {
     private:
 
-        using Base = details::ObservableImpl<IObserver, Enclosing>;
-        using ObserverElement = typename Base::ObserverElement;
+        using ObserverElement = Observer<IObserver>;
+        using ObserverList = quick_list<ObserverElement, observer_link>;
 
     public:
 
         Observable() = default;
-        ~Observable() = default;
+
+        ~Observable()
+        {
+            clearObservers();
+        }
 
         Observable(const Observable& other) = delete;
-        Observable(Observable&& other) = default;
+
+        Observable(Observable&& other) : m_observers(std::move(other.m_observers)) {}
 
         Observable& operator = (const Observable& other) = delete;
-        Observable& operator = (Observable&& other) noexcept = default;
 
-        using Base::subscribe;
-        using Base::unsubscribe;
-        using Base::empty;
-        using Base::size;
+        Observable& operator = (Observable&& other) noexcept
+        {
+            clearObservers();
+            m_observers = std::move(other.m_observers);
+            return *this;
+        }
+
+        void subscribe(ObserverElement* p_observer)
+        {
+            m_observers.push_back(p_observer);
+        }
+
+        void unsubscribe(ObserverElement* p_observer)
+        {
+            p_observer->unsubscribeSelf();
+        }
+
+        bool empty() const
+        {
+            return m_observers.empty();
+        }
+
+        auto size() const
+        {
+            return m_observers.size();
+        }
 
     protected:
 
@@ -41,7 +71,11 @@ namespace awl
         void notify(void (IObserver::* func)(Params ...), const Args& ... args)
             requires (std::invocable<decltype(func), IObserver*, const Args&...>)
         {
-            Base::notifyImpl([&](ObserverElement* p_observer) { (static_cast<IObserver*>(p_observer)->*func)(args ...); });
+            notifyLoop([&](ObserverElement* p_observer)
+            {
+                (static_cast<IObserver*>(p_observer)->*func)(args ...);
+                return true;
+            });
         }
 
         // It is not clear enough if we really need const notify methods like this:
@@ -55,9 +89,49 @@ namespace awl
             std::invocable<decltype(func), IObserver*, const Args&...>
             )
         {
-            return Base::notifyWhileTrueImpl([&](ObserverElement* p_observer) { return (static_cast<IObserver*>(p_observer)->*func)(args ...); });
+            return notifyLoop([&](ObserverElement* p_observer)
+            {
+                return (static_cast<IObserver*>(p_observer)->*func)(args ...);
+            });
         }
 
         friend Enclosing;
+
+    private:
+
+        template <class Callable>
+        bool notifyLoop(Callable&& call)
+            requires (
+                std::invocable<Callable&, ObserverElement*> &&
+                std::convertible_to<std::invoke_result_t<Callable&, ObserverElement*>, bool>
+            )
+        {
+            for (typename ObserverList::iterator i = m_observers.begin(); i != m_observers.end(); )
+            {
+                //p_observer can delete itself or unsubscribe while iterating over the list so we use postfix ++
+                ObserverElement* p_observer = *(i++);
+
+                if (!static_cast<bool>(call(p_observer)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //If the observable is deleted before its observers,
+        //we remove them from the list, otherwise they will think that they are included and
+        //their destructors will delete them from already destroyed list.
+        //So we can't use m_observers.clear() here because it only clears list's head.
+        void clearObservers()
+        {
+            while (!m_observers.empty())
+            {
+                m_observers.pop_front();
+            }
+        }
+
+        ObserverList m_observers;
     };
 }
--- Tests/Experimental/ObservableFunction.h
+++ Tests/Experimental/ObservableFunction.h
@@ -5,40 +5,68 @@
 
 #pragma once
 
-#include "Tests/Experimental/ObserverFunction.h"
 #include "Awl/Observable.h"
+#include "Tests/Experimental/ObserverFunction.h"
 
+#include <concepts>
 #include <functional>
+#include <type_traits>
+#include <utility>
 
 namespace awl
 {
     // Storing this pointer in a std::function with std::bind, for example, prevents the observers from being movable,
     // so this class is experimental and is not used in production.
     template <class Result, class... Params, class Enclosing>
-    class Observable<std::function<Result(Params...)>, Enclosing> :
-        private details::ObservableImpl<std::function<Result(Params...)>, Enclosing>
+    class Observable<std::function<Result(Params...)>, Enclosing>
     {
     private:
 
         using Function = std::function<Result(Params...)>;
-        using Base = details::ObservableImpl<Function, Enclosing>;
-        using ObserverElement = typename Base::ObserverElement;
+        using ObserverElement = Observer<Function>;
+        using ObserverList = quick_list<ObserverElement, observer_link>;
 
     public:
 
         Observable() = default;
-        ~Observable() = default;
+
+        ~Observable()
+        {
+            clearObservers();
+        }
 
         Observable(const Observable& other) = delete;
-        Observable(Observable&& other) = default;
+
+        Observable(Observable&& other) : m_observers(std::move(other.m_observers)) {}
 
         Observable& operator = (const Observable& other) = delete;
-        Observable& operator = (Observable&& other) noexcept = default;
 
-        using Base::subscribe;
-        using Base::unsubscribe;
-        using Base::empty;
-        using Base::size;
+        Observable& operator = (Observable&& other) noexcept
+        {
+            clearObservers();
+            m_observers = std::move(other.m_observers);
+            return *this;
+        }
+
+        void subscribe(ObserverElement* p_observer)
+        {
+            m_observers.push_back(p_observer);
+        }
+
+        void unsubscribe(ObserverElement* p_observer)
+        {
+            p_observer->unsubscribeSelf();
+        }
+
+        bool empty() const
+        {
+            return m_observers.empty();
+        }
+
+        auto size() const
+        {
+            return m_observers.size();
+        }
 
     protected:
 
@@ -46,7 +74,11 @@ namespace awl
         void notify(const Args& ... args)
             requires (std::invocable<Function&, const Args&...>)
         {
-            Base::notifyImpl([&](ObserverElement* p_observer) { p_observer->function()(args ...); });
+            notifyLoopImpl([&](ObserverElement* p_observer)
+            {
+                p_observer->function()(args ...);
+                return true;
+            });
         }
 
         template<typename ... Args>
@@ -56,9 +88,41 @@ namespace awl
                 std::invocable<Function&, const Args&...>
             )
         {
-            return Base::notifyWhileTrueImpl([&](ObserverElement* p_observer) { return p_observer->function()(args ...); });
+            return notifyLoopImpl([&](ObserverElement* p_observer) { return p_observer->function()(args ...); });
         }
 
         friend Enclosing;
+
+    private:
+
+        template <class Callable>
+        bool notifyLoopImpl(Callable&& call)
+            requires (
+                std::invocable<Callable&, ObserverElement*> &&
+                std::convertible_to<std::invoke_result_t<Callable&, ObserverElement*>, bool>
+            )
+        {
+            for (typename ObserverList::iterator i = m_observers.begin(); i != m_observers.end(); )
+            {
+                ObserverElement* p_observer = *(i++);
+
+                if (!static_cast<bool>(call(p_observer)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        void clearObservers()
+        {
+            while (!m_observers.empty())
+            {
+                m_observers.pop_front();
+            }
+        }
+
+        ObserverList m_observers;
     };
 }
