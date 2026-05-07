fad0a5b079991e3dc111e67dc49821b46d752954
Dmitriano <windappdev@gmail.com>
2026-05-07 14:08:20 +0300

    ConsoleLogger cc

---
+++ Awl/ConsoleLogger.cpp
@@ -0,0 +1,68 @@
+#include "Awl/ConsoleLogger.h"
+
+#include "Awl/String.h"
+#include "Awl/Time.h"
+
+#include <chrono>
+#include <source_location>
+#include <string_view>
+#include <utility>
+
+#if defined(__APPLE__)
+#include <ctime>
+#endif
+
+namespace
+{
+    std::string_view fileName(const char* path)
+    {
+        const std::string_view full_path(path);
+        const size_t pos = full_path.find_last_of("/\\");
+        return pos == std::string_view::npos ? full_path : full_path.substr(pos + 1);
+    }
+}
+
+namespace awl
+{
+    ConsoleLogger::ConsoleLogger(
+        awl::ostream& out,
+        std::string min_level) :
+        m_out(out),
+        m_minSeverity(log_level_severity(std::move(min_level)))
+    {
+    }
+
+    bool ConsoleLogger::enabled(const std::string& level) const
+    {
+        return log_level_severity(level) >= m_minSeverity;
+    }
+
+    void ConsoleLogger::log(const std::string& level, const LogString& message)
+    {
+        awl::ostringstream temp_out;
+        const std::source_location location = message.location();
+
+        // A quick fix to prevent compiler error "'to_chars' is unavailable: introduced in iOS 16.3"
+        // with IPHONEOS_DEPLOYMENT_TARGET = 14.0 on Apple platform.
+        // Alternatively IPHONEOS_DEPLOYMENT_TARGET can be set to 16.6, for example.
+#if defined(__APPLE__)
+        auto tp = std::chrono::system_clock::now();
+        const std::time_t t = std::chrono::system_clock::to_time_t(tp);
+        temp_out << std::ctime(&t);
+#else
+        temp_out << std::chrono::system_clock::now();
+#endif
+
+        temp_out << _T('\t')
+                 << level
+                 << _T('\t')
+                 << awl::fromAString(std::string(fileName(location.file_name())))
+                 << _T(':')
+                 << location.line()
+                 << _T('\t')
+                 << message.str()
+                 << _T('\n');
+
+        m_out << temp_out.str();
+    }
+}
--- Awl/ConsoleLogger.h
+++ Awl/ConsoleLogger.h
@@ -7,17 +7,8 @@
 
 #include "Logger.h"
 #include "Awl/StdConsole.h"
-#include "Awl/Time.h"
-#include "Awl/String.h"
 
-#include <chrono>
 #include <string>
-#include <string_view>
-#include <utility>
-
-#if defined(__APPLE__)
-#include <ctime>
-#endif
 
 namespace awl
 {
@@ -29,53 +20,13 @@ namespace awl
 
         ConsoleLogger(
             awl::ostream& out = awl::cout(),
-            std::string min_level = LogLevel::Debug) :
-            m_out(out),
-            m_minSeverity(log_level_severity(std::move(min_level)))
-        {}
+            std::string min_level = LogLevel::Debug);
 
-        bool enabled(const std::string& level) const override
-        {
-            return log_level_severity(level) >= m_minSeverity;
-        }
+        bool enabled(const std::string& level) const override;
 
-        void log(const std::string& level, const LogString& message) override
-        {
-            awl::ostringstream temp_out;
-            const std::source_location location = message.location();
-
-            // A quick fix to prevent compiler error "‘to_chars’ is unavailable: introduced in iOS 16.3"
-            // with IPHONEOS_DEPLOYMENT_TARGET = 14.0 on Apple platform.
-            // Alternatively IPHONEOS_DEPLOYMENT_TARGET can be set to 16.6, for example.
-#if defined(__APPLE__)
-            auto tp = std::chrono::system_clock::now();
-            const std::time_t t = std::chrono::system_clock::to_time_t(tp);
-            temp_out << std::ctime(&t);
-#else
-            temp_out << std::chrono::system_clock::now();
-#endif
-
-            temp_out << _T('\t')
-                     << level
-                     << _T('\t')
-                     << awl::fromAString(std::string(fileName(location.file_name())))
-                     << _T(':')
-                     << location.line()
-                     << _T('\t')
-                     << message.str()
-                     << _T('\n');
-
-            m_out << temp_out.str();
-        }
+        void log(const std::string& level, const LogString& message) override;
 
     private:
-
-        static std::string_view fileName(const char* path)
-        {
-            const std::string_view full_path(path);
-            const size_t pos = full_path.find_last_of("/\\");
-            return pos == std::string_view::npos ? full_path : full_path.substr(pos + 1);
-        }
 
         awl::ostream& m_out;
         std::size_t m_minSeverity;
