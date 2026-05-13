9a6897055c4092e90a6ae17e4861a2f22bc66f33
Dmitriano <windappdev@gmail.com>
2026-05-13 10:50:14 +0300

    std::move_only_function

--- Awl/ITransaction.h
+++ Awl/ITransaction.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <functional>
+#include <memory>
+#include <utility>
+
 namespace awl
 {
     class ITransaction
@@ -10,4 +14,15 @@ namespace awl
 
         virtual void commit() = 0;
     };
+
+    inline std::move_only_function<void()> wrapInTransaction(
+        std::unique_ptr<ITransaction> transaction,
+        std::move_only_function<void()> func)
+    {
+        return [transaction = std::move(transaction), func = std::move(func)]() mutable
+        {
+            func();
+            transaction->commit();
+        };
+    }
 }
