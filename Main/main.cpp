600e8f63c4e85da11098b67ccfb9ebf9164baf55
Dmitriano <windappdev@gmail.com>
2026-05-15 10:29:37 +0300

    configureAssertReporting()

--- Main/main.cpp
+++ Main/main.cpp
@@ -8,9 +8,34 @@
 #include "Awl/String.h"
 #include "Awl/StdConsole.h"
 
+#include <cstdlib>
+
+#ifdef _MSC_VER
+#include <crtdbg.h>
+#endif
+
 #ifndef _tmain
 #define _tmain main
 #endif
+
+namespace
+{
+    void configureAssertReporting()
+    {
+#if defined(_MSC_VER) && defined(_DEBUG)
+        _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+        _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+
+        _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+        _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+
+        _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE | _CRTDBG_MODE_DEBUG);
+        _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+
+        _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+#endif
+    }
+}
 
 #ifdef AWL_ANSI_CMD_CHAR
 int main(int argc, char* argv[])
@@ -18,6 +43,8 @@ int main(int argc, char* argv[])
 int _tmain(int argc, awl::Char * argv[])
 #endif
 {
+    configureAssertReporting();
+
     try
     {
         return awl::testing::run(argc, argv);
