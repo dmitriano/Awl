5dcebabc6e2102c7d4a2d64a6132ff2c428b4f11
Dmitriano <windappdev@gmail.com>
2026-05-18 17:19:17 +0300

    Inline reflectable formatting into std formatter

--- Awl/ReflectableFormatter.h
+++ Awl/ReflectableFormatter.h
@@ -19,39 +19,7 @@
 #include <tuple>
 
 namespace awl
-{
-    namespace detail
-    {
-        template <class CharT, reflectable T, class Field, class Index>
-        void appendReflectableField(std::basic_ostream<CharT>& out, const Field& field, Index index, awl::basic_separator<CharT>& sep)
-        {
-            out << sep;
-
-            static constexpr auto format_string = awl::string_from_ascii<CharT>("{}={}");
-
-            out << std::format(format_string, T::member_names()[index], field);
-        }
-    }
-
-    template <class CharT, reflectable T>
-    std::basic_ostream<CharT>& writeReflectable(std::basic_ostream<CharT>& out, const T& val)
-    {
-        const auto tuple = object_as_tuple(val);
-
-        out << static_cast<CharT>('{');
-
-        awl::basic_separator<CharT> sep(static_cast<CharT>(','));
-
-        awl::for_each_index(tuple, [&out, &sep](const auto& field, auto index)
-        {
-            detail::appendReflectableField<CharT, T>(out, field, index, sep);
-        });
-
-        out << static_cast<CharT>('}');
-
-        return out;
-    }
-}
+{}
 
 namespace std
 {
@@ -61,8 +29,21 @@ namespace std
         template <class FormatContext>
         auto format(const T& val, FormatContext& ctx) const
         {
+            const auto tuple = awl::object_as_tuple(val);
             std::basic_ostringstream<CharT> out;
-            awl::writeReflectable(out, val);
+
+            out << static_cast<CharT>('{');
+
+            awl::basic_separator<CharT> sep(static_cast<CharT>(','));
+
+            awl::for_each_index(tuple, [&out, &sep](const auto& field, auto index)
+            {
+                static constexpr auto format_string = awl::string_from_ascii<CharT>("{}={}");
+
+                out << sep << std::format(format_string, T::member_names()[index], field);
+            });
+
+            out << static_cast<CharT>('}');
 
             return formatter<std::basic_string<CharT>, CharT>::format(out.str(), ctx);
         }
