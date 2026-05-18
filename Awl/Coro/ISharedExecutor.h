b010e0f3be2e0f651928fe255104707787ad5350
Dmitriano <windappdev@gmail.com>
2026-05-19 01:19:05 +0300

    Remove shared executor options and transaction dispatcher

--- Awl/Coro/ISharedExecutor.h
+++ Awl/Coro/ISharedExecutor.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Awl/BitMap.h"
 #include "Awl/Coro/Awaitable.h"
 #include "Awl/Coro/Task.h"
 
@@ -16,20 +15,16 @@ namespace awl::coro
     {
     public:
 
-        AWL_SEQUENTIAL_ENUM(Option, Read, Write, Transaction)
-
-        using Options = awl::bitmap<Option, OptionTraits::Count>;
-
         virtual ~ISharedExecutor() = default;
 
-        virtual Awaitable<void> execute(std::move_only_function<void()> func, Options options) = 0;
+        virtual Awaitable<void> execute(std::move_only_function<void()> func) = 0;
     };
 
     class ISharedDispatcher : public ISharedExecutor
     {
     public:
 
-        virtual void post(std::move_only_function<void()> func, Options options) = 0;
+        virtual void post(std::move_only_function<void()> func) = 0;
 
         virtual void join() = 0;
     };
@@ -43,8 +38,7 @@ namespace awl::coro
     template <class Func>
     Task<detail::shared_executor_result_t<Func>> execute(
         ISharedExecutor& executor,
-        Func func,
-        ISharedExecutor::Options options)
+        Func func)
     {
         using Result = std::invoke_result_t<Func&>;
 
@@ -53,8 +47,7 @@ namespace awl::coro
             co_await executor.execute([func = std::move(func)] mutable
                 {
                     std::invoke(func);
-                },
-                options);
+                });
 
             co_return;
         }
@@ -66,8 +59,7 @@ namespace awl::coro
             co_await executor.execute([func = std::move(func), &result]() mutable
                 {
                     result.emplace(std::invoke(func));
-                },
-                options);
+                });
 
             co_return std::move(*result);
         }
@@ -76,20 +68,17 @@ namespace awl::coro
     template <class Func>
     Task<detail::shared_executor_result_t<Func>> execute(
         const std::shared_ptr<ISharedExecutor>& executor,
-        Func func,
-        ISharedExecutor::Options options)
+        Func func)
     {
         if constexpr (std::is_void_v<std::invoke_result_t<Func&>>)
         {
-            co_await execute(*executor, std::move(func), options);
+            co_await execute(*executor, std::move(func));
 
             co_return;
         }
         else
         {
-            co_return co_await execute(*executor, std::move(func), options);
+            co_return co_await execute(*executor, std::move(func));
         }
     }
 }
-
-AWL_ENUM_TRAITS(awl::coro::ISharedExecutor, Option)
