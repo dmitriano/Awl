b5d1040c0c95e7f5060d79f93f5ca66c96dc1a18
Dmitriano <windappdev@gmail.com>
2026-05-31 20:32:19 +0300

    catch (const awl::coro::ChannelClosedException&)

---
+++ Awl/Coro/Channel.h
@@ -0,0 +1,432 @@
+#pragma once
+
+#include "Awl/Coro/Task.h"
+#include "Awl/Exception.h"
+
+#include <algorithm>
+#include <coroutine>
+#include <cstddef>
+#include <deque>
+#include <exception>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <utility>
+#include <vector>
+
+namespace awl::coro
+{
+    AWL_DEFINE_EXCEPTION(ChannelClosedException)
+
+    template <class T>
+    class Channel
+    {
+    private:
+
+        class ReceiveOperation;
+        class SendOperation;
+
+        struct State
+        {
+            explicit State(const size_t init_capacity) :
+                capacity(init_capacity)
+            {}
+
+            std::mutex mutex;
+            bool open = true;
+            size_t capacity;
+            std::deque<T> values;
+            std::deque<ReceiveOperation*> receivers;
+            std::deque<SendOperation*> senders;
+        };
+
+    public:
+
+        explicit Channel(size_t capacity = 0) :
+            _state(std::make_shared<State>(capacity))
+        {}
+
+        Channel(const Channel&) = delete;
+
+        Channel(Channel&&) = delete;
+
+        Channel& operator = (const Channel&) = delete;
+
+        Channel& operator = (Channel&&) = delete;
+
+        ~Channel()
+        {
+            close();
+        }
+
+        bool isOpen() const
+        {
+            std::lock_guard lock(_state->mutex);
+
+            return _state->open;
+        }
+
+        void close()
+        {
+            std::vector<std::coroutine_handle<>> continuations;
+
+            {
+                std::lock_guard lock(_state->mutex);
+
+                if (_state->open)
+                {
+                    _state->open = false;
+
+                    while (!_state->receivers.empty())
+                    {
+                        ReceiveOperation* receiver = _state->receivers.front();
+                        _state->receivers.pop_front();
+
+                        receiver->setException(makeClosedException(), continuations);
+                    }
+
+                    while (!_state->senders.empty())
+                    {
+                        SendOperation* sender = _state->senders.front();
+                        _state->senders.pop_front();
+
+                        sender->setException(makeClosedException(), continuations);
+                    }
+                }
+            }
+
+            resume(continuations);
+        }
+
+        Task<void> asyncSend(T val)
+        {
+            co_await SendOperation(_state, std::move(val));
+        }
+
+        Task<T> asyncReceive()
+        {
+            co_return co_await ReceiveOperation(_state);
+        }
+
+    private:
+
+        static std::exception_ptr makeClosedException()
+        {
+            return std::make_exception_ptr(ChannelClosedException("Channel is closed."));
+        }
+
+        static void resume(const std::vector<std::coroutine_handle<>>& continuations)
+        {
+            for (std::coroutine_handle<> continuation : continuations)
+            {
+                continuation.resume();
+            }
+        }
+
+        template <class Operation>
+        static void erase(std::deque<Operation*>& operations, Operation* operation);
+
+        static void complete(SendOperation& sender, std::vector<std::coroutine_handle<>>& continuations);
+
+        static void complete(
+            ReceiveOperation& receiver,
+            T val,
+            std::vector<std::coroutine_handle<>>& continuations);
+
+        static void moveWaitingSenderToBuffer(
+            State& state,
+            std::vector<std::coroutine_handle<>>& continuations);
+
+        class ReceiveOperation
+        {
+        public:
+
+            explicit ReceiveOperation(std::shared_ptr<State> init_state) :
+                state(std::move(init_state))
+            {}
+
+            ReceiveOperation(const ReceiveOperation&) = delete;
+
+            ReceiveOperation(ReceiveOperation&&) = delete;
+
+            ReceiveOperation& operator = (const ReceiveOperation&) = delete;
+
+            ReceiveOperation& operator = (ReceiveOperation&&) = delete;
+
+            ~ReceiveOperation()
+            {
+                unsubscribe();
+            }
+
+            bool await_ready() const noexcept
+            {
+                return false;
+            }
+
+            bool await_suspend(std::coroutine_handle<> h)
+            {
+                coroutine = h;
+
+                std::vector<std::coroutine_handle<>> continuations;
+                bool suspended = false;
+
+                {
+                    std::lock_guard lock(state->mutex);
+
+                    if (!state->values.empty())
+                    {
+                        value = std::move(state->values.front());
+                        state->values.pop_front();
+                        coroutine = nullptr;
+
+                        moveWaitingSenderToBuffer(*state, continuations);
+                    }
+                    else if (!state->senders.empty())
+                    {
+                        SendOperation* sender = state->senders.front();
+                        state->senders.pop_front();
+
+                        value = std::move(*sender->value);
+                        sender->value.reset();
+                        coroutine = nullptr;
+
+                        complete(*sender, continuations);
+                    }
+                    else if (!state->open)
+                    {
+                        exception = makeClosedException();
+                        coroutine = nullptr;
+                    }
+                    else
+                    {
+                        state->receivers.push_back(this);
+                        registered = true;
+                        suspended = true;
+                    }
+                }
+
+                resume(continuations);
+
+                return suspended;
+            }
+
+            T await_resume()
+            {
+                unsubscribe();
+
+                if (exception)
+                {
+                    std::rethrow_exception(exception);
+                }
+
+                return std::move(*value);
+            }
+
+            void setException(
+                std::exception_ptr init_exception,
+                std::vector<std::coroutine_handle<>>& continuations)
+            {
+                registered = false;
+                exception = std::move(init_exception);
+
+                if (std::coroutine_handle<> continuation = std::exchange(coroutine, nullptr))
+                {
+                    continuations.push_back(continuation);
+                }
+            }
+
+            std::shared_ptr<State> state;
+            std::optional<T> value;
+            std::exception_ptr exception;
+            std::coroutine_handle<> coroutine = nullptr;
+            bool registered = false;
+
+        private:
+
+            void unsubscribe()
+            {
+                if (registered)
+                {
+                    std::lock_guard lock(state->mutex);
+
+                    erase(state->receivers, this);
+                    registered = false;
+                    coroutine = nullptr;
+                }
+            }
+        };
+
+        class SendOperation
+        {
+        public:
+
+            SendOperation(std::shared_ptr<State> init_state, T init_value) :
+                state(std::move(init_state)),
+                value(std::move(init_value))
+            {}
+
+            SendOperation(const SendOperation&) = delete;
+
+            SendOperation(SendOperation&&) = delete;
+
+            SendOperation& operator = (const SendOperation&) = delete;
+
+            SendOperation& operator = (SendOperation&&) = delete;
+
+            ~SendOperation()
+            {
+                unsubscribe();
+            }
+
+            bool await_ready() const noexcept
+            {
+                return false;
+            }
+
+            bool await_suspend(std::coroutine_handle<> h)
+            {
+                coroutine = h;
+
+                std::vector<std::coroutine_handle<>> continuations;
+                bool suspended = false;
+
+                {
+                    std::lock_guard lock(state->mutex);
+
+                    if (!state->open)
+                    {
+                        exception = makeClosedException();
+                        coroutine = nullptr;
+                    }
+                    else if (!state->receivers.empty())
+                    {
+                        ReceiveOperation* receiver = state->receivers.front();
+                        state->receivers.pop_front();
+
+                        complete(*receiver, std::move(*value), continuations);
+                        value.reset();
+                        coroutine = nullptr;
+                    }
+                    else if (state->capacity != 0 && state->values.size() < state->capacity)
+                    {
+                        state->values.push_back(std::move(*value));
+                        value.reset();
+                        coroutine = nullptr;
+                    }
+                    else
+                    {
+                        state->senders.push_back(this);
+                        registered = true;
+                        suspended = true;
+                    }
+                }
+
+                resume(continuations);
+
+                return suspended;
+            }
+
+            void await_resume()
+            {
+                unsubscribe();
+
+                if (exception)
+                {
+                    std::rethrow_exception(exception);
+                }
+            }
+
+            void setException(
+                std::exception_ptr init_exception,
+                std::vector<std::coroutine_handle<>>& continuations)
+            {
+                registered = false;
+                exception = std::move(init_exception);
+
+                if (std::coroutine_handle<> continuation = std::exchange(coroutine, nullptr))
+                {
+                    continuations.push_back(continuation);
+                }
+            }
+
+            std::shared_ptr<State> state;
+            std::optional<T> value;
+            std::exception_ptr exception;
+            std::coroutine_handle<> coroutine = nullptr;
+            bool registered = false;
+
+        private:
+
+            void unsubscribe()
+            {
+                if (registered)
+                {
+                    std::lock_guard lock(state->mutex);
+
+                    erase(state->senders, this);
+                    registered = false;
+                    coroutine = nullptr;
+                }
+            }
+        };
+
+        std::shared_ptr<State> _state;
+    };
+
+    template <class T>
+    template <class Operation>
+    void Channel<T>::erase(std::deque<Operation*>& operations, Operation* operation)
+    {
+        const auto i = std::ranges::find(operations, operation);
+
+        if (i != operations.end())
+        {
+            operations.erase(i);
+        }
+    }
+
+    template <class T>
+    void Channel<T>::complete(
+        typename Channel<T>::SendOperation& sender,
+        std::vector<std::coroutine_handle<>>& continuations)
+    {
+        sender.registered = false;
+
+        if (std::coroutine_handle<> continuation = std::exchange(sender.coroutine, nullptr))
+        {
+            continuations.push_back(continuation);
+        }
+    }
+
+    template <class T>
+    void Channel<T>::complete(
+        typename Channel<T>::ReceiveOperation& receiver,
+        T val,
+        std::vector<std::coroutine_handle<>>& continuations)
+    {
+        receiver.registered = false;
+        receiver.value = std::move(val);
+
+        if (std::coroutine_handle<> continuation = std::exchange(receiver.coroutine, nullptr))
+        {
+            continuations.push_back(continuation);
+        }
+    }
+
+    template <class T>
+    void Channel<T>::moveWaitingSenderToBuffer(
+        typename Channel<T>::State& state,
+        std::vector<std::coroutine_handle<>>& continuations)
+    {
+        if (state.open && state.capacity != 0 && !state.senders.empty() && state.values.size() < state.capacity)
+        {
+            typename Channel<T>::SendOperation* sender = state.senders.front();
+            state.senders.pop_front();
+
+            state.values.push_back(std::move(*sender->value));
+            sender->value.reset();
+
+            complete(*sender, continuations);
+        }
+    }
+}
---
+++ Tests/CoroChannelTest.cpp
@@ -0,0 +1,218 @@
+#include "Awl/Coro/Channel.h"
+#include "Awl/Coro/Job.h"
+#include "Awl/Testing/UnitTest.h"
+
+#include <string>
+#include <utility>
+
+namespace
+{
+    template <class T>
+    awl::coro::Job send(
+        awl::coro::Channel<T>& channel,
+        T value,
+        bool& sent,
+        bool& closed)
+    {
+        try
+        {
+            co_await channel.asyncSend(std::move(value));
+            sent = true;
+        }
+        catch (const awl::coro::ChannelClosedException&)
+        {
+            closed = true;
+        }
+    }
+
+    template <class T>
+    awl::coro::Job receive(
+        awl::coro::Channel<T>& channel,
+        T& value,
+        bool& received,
+        bool& closed)
+    {
+        try
+        {
+            value = co_await channel.asyncReceive();
+            received = true;
+        }
+        catch (const awl::coro::ChannelClosedException&)
+        {
+            closed = true;
+        }
+    }
+}
+
+AWL_TEST(CoroChannel_Rendezvous)
+{
+    AWL_UNUSED_CONTEXT;
+
+    awl::coro::Channel<int> channel;
+
+    int value = 0;
+    bool received = false;
+    bool receive_closed = false;
+    awl::coro::Job receiver = receive(channel, value, received, receive_closed);
+
+    AWL_ASSERT(!receiver.done());
+
+    bool sent = false;
+    bool send_closed = false;
+    awl::coro::Job sender = send(channel, 7, sent, send_closed);
+
+    AWL_ASSERT(sender.done());
+    AWL_ASSERT(receiver.done());
+    AWL_ASSERT(sent);
+    AWL_ASSERT(!send_closed);
+    AWL_ASSERT(received);
+    AWL_ASSERT(!receive_closed);
+    AWL_ASSERT_EQUAL(7, value);
+}
+
+AWL_TEST(CoroChannel_Buffered)
+{
+    AWL_UNUSED_CONTEXT;
+
+    awl::coro::Channel<std::string> channel(2);
+
+    bool first_sent = false;
+    bool first_send_closed = false;
+    awl::coro::Job first_sender = send(channel, std::string("first"), first_sent, first_send_closed);
+
+    bool second_sent = false;
+    bool second_send_closed = false;
+    awl::coro::Job second_sender = send(channel, std::string("second"), second_sent, second_send_closed);
+
+    AWL_ASSERT(first_sender.done());
+    AWL_ASSERT(second_sender.done());
+    AWL_ASSERT(first_sent);
+    AWL_ASSERT(second_sent);
+
+    std::string first_value;
+    bool first_received = false;
+    bool first_receive_closed = false;
+    awl::coro::Job first_receiver = receive(channel, first_value, first_received, first_receive_closed);
+
+    std::string second_value;
+    bool second_received = false;
+    bool second_receive_closed = false;
+    awl::coro::Job second_receiver = receive(channel, second_value, second_received, second_receive_closed);
+
+    AWL_ASSERT(first_receiver.done());
+    AWL_ASSERT(second_receiver.done());
+    AWL_ASSERT(first_received);
+    AWL_ASSERT(second_received);
+    AWL_ASSERT_EQUAL(std::string("first"), first_value);
+    AWL_ASSERT_EQUAL(std::string("second"), second_value);
+}
+
+AWL_TEST(CoroChannel_BackPressure)
+{
+    AWL_UNUSED_CONTEXT;
+
+    awl::coro::Channel<int> channel(1);
+
+    bool first_sent = false;
+    bool first_send_closed = false;
+    awl::coro::Job first_sender = send(channel, 1, first_sent, first_send_closed);
+
+    bool second_sent = false;
+    bool second_send_closed = false;
+    awl::coro::Job second_sender = send(channel, 2, second_sent, second_send_closed);
+
+    AWL_ASSERT(first_sender.done());
+    AWL_ASSERT(!second_sender.done());
+    AWL_ASSERT(first_sent);
+    AWL_ASSERT(!second_sent);
+
+    int first_value = 0;
+    bool first_received = false;
+    bool first_receive_closed = false;
+    awl::coro::Job first_receiver = receive(channel, first_value, first_received, first_receive_closed);
+
+    AWL_ASSERT(first_receiver.done());
+    AWL_ASSERT(second_sender.done());
+    AWL_ASSERT(second_sent);
+    AWL_ASSERT_EQUAL(1, first_value);
+
+    int second_value = 0;
+    bool second_received = false;
+    bool second_receive_closed = false;
+    awl::coro::Job second_receiver = receive(channel, second_value, second_received, second_receive_closed);
+
+    AWL_ASSERT(second_receiver.done());
+    AWL_ASSERT(second_received);
+    AWL_ASSERT_EQUAL(2, second_value);
+}
+
+AWL_TEST(CoroChannel_CloseWaitingReceiver)
+{
+    AWL_UNUSED_CONTEXT;
+
+    awl::coro::Channel<int> channel;
+
+    int value = 0;
+    bool received = false;
+    bool receive_closed = false;
+    awl::coro::Job receiver = receive(channel, value, received, receive_closed);
+
+    AWL_ASSERT(!receiver.done());
+
+    channel.close();
+
+    AWL_ASSERT(receiver.done());
+    AWL_ASSERT(!channel.isOpen());
+    AWL_ASSERT(!received);
+    AWL_ASSERT(receive_closed);
+}
+
+AWL_TEST(CoroChannel_CloseKeepsBufferedValues)
+{
+    AWL_UNUSED_CONTEXT;
+
+    awl::coro::Channel<int> channel(1);
+
+    bool sent = false;
+    bool send_closed = false;
+    awl::coro::Job sender = send(channel, 3, sent, send_closed);
+
+    AWL_ASSERT(sender.done());
+
+    channel.close();
+
+    int value = 0;
+    bool received = false;
+    bool receive_closed = false;
+    awl::coro::Job receiver = receive(channel, value, received, receive_closed);
+
+    AWL_ASSERT(receiver.done());
+    AWL_ASSERT(received);
+    AWL_ASSERT(!receive_closed);
+    AWL_ASSERT_EQUAL(3, value);
+
+    int closed_value = 0;
+    bool closed_received = false;
+    bool closed_receive_closed = false;
+    awl::coro::Job closed_receiver = receive(channel, closed_value, closed_received, closed_receive_closed);
+
+    AWL_ASSERT(closed_receiver.done());
+    AWL_ASSERT(!closed_received);
+    AWL_ASSERT(closed_receive_closed);
+}
+
+AWL_TEST(CoroChannel_SendToClosedChannel)
+{
+    AWL_UNUSED_CONTEXT;
+
+    awl::coro::Channel<int> channel;
+    channel.close();
+
+    bool sent = false;
+    bool send_closed = false;
+    awl::coro::Job sender = send(channel, 5, sent, send_closed);
+
+    AWL_ASSERT(sender.done());
+    AWL_ASSERT(!sent);
+    AWL_ASSERT(send_closed);
+}
